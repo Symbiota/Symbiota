6438c41a9fd19544c79dda6dcbf942f82946811c
glyptostrob0ides <lparadiso@nybg.org>
2021-02-17 15:01:44 -0500

    footer update

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,43 @@
-    	</td>
+<table>
+	<tr>
+		<td class="footer1" colspan="5">
+			<div style="width:auto;">
+				<div style="width:300px;padding:0px 20px;display:table-cell;vertical-align:middle;">
+					<a href="https://www.chicagobotanic.org" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/cbglogo.jpg" style="width:100px" />
+					</a>
+				</div>
+				<div style="width:300px;padding:0px 20px;display:table-cell;vertical-align:middle;">
+					<a href="https://www.botanicgardens.org" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/denverlogo.jpg" style="width:100px" /></a>
+				</div>
+				<div style="width:300px;padding:0px 20px;display:table-cell;vertical-align:middle;">
+					<a href="https://www.dbg.org" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/desertlogo.jpg" style="width:100px" /></a>
+				</div>
+				<div style="width:300px;padding:0px 20px;display:table-cell;vertical-align:middle;">
+					<a href="https://www.nybg.org" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/nybglogo.png" style="width:100px" /></a>
+				</div>
+				<div style="width:300px;padding:0px 20px;display:table-cell;vertical-align:middle;">
+					<a href="https://www.selby.org" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/selbygardens.png" style="width:100px" /></a>
+				</div>
+			</div>
+		</td>
 	</tr>
 	<tr>
-		<td id="footer" colspan="3">
-			<p>
-				<a href="https://www.chicagobotanic.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/cbglogo.jpg" style="width:70px" /></a>
-				<a href="https://www.botanicgardens.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/denverlogo.jpg" style="width:70px" /></a>
-				<a href="https://www.dbg.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/desertlogo.jpg" style="width:70px" /></a>
-				<a href="https://www.nybg.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/nybglogo.png" style="width:70px" /></a>
-				<a href="https://www.selby.org" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/selbygardens.png" style="width:70px" /></a>
-			</p>
-			<p>
-				<a href="https://www.imls.gov" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/imlslogo.jpg" style="width:70px" /></a>
-				<a href="https://biokic.asu.edu" target="_blank"><img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.jpg" style="width:70px" /></a>
-			</p>
-			<div style="text-align:center;margin-bottom:15px">This project made possible by the Institute of Museum and Library Services award MG-70-19-0057-19 <a href="https://imls.gov/grants/awarded/mg-70-19-0057-19-0" target="_blank"></a></div>
+		<td class="footer2" colspan="2">
+			<div style="width:auto;margin-left:auto;margin-right:auto;margin-bottom:15px;display:table;">
+				<div style="width:300px;display:table-cell;vertical-align:middle;">
+					<a href="https://www.imls.gov" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/imlslogo.jpg" style="width:100px" /></a>
+				</div>
+				<div style="width:300px;display:table-cell;vertical-align:middle;">
+					<a href="https://biokic.asu.edu" target="_blank">
+					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.jpg" style="width:100px" /></a>
+				</div>
+			</div>
 		</td>
 	</tr>
 </table>
