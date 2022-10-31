5cd9d5e77d65eb27583fd153fbf42c8c842b50db
Lindsay Walker <ljwalker@users.noreply.github.com>
2022-10-31 16:33:11 -0700

    Footer images fixed

--- includes/footer.php
+++ includes/footer.php
@@ -1,19 +1,19 @@
 <footer>
         <div class="logo-gallery">
                 <a href="https://www.nsf.gov" target="_blank" title="National Science Foundation">
-                        <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif" />
+                        <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_nsf.gif"/>
                 </a>
                 <a href="https://www.dmns.org/" target="_blank" title="DMNS">
-                        <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_dmns.png" />
+                        <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_dmns.png"/>
                 </a>
                 <a href="https://www.univ-antananarivo.mg/" target="_blank" title="UA">
-                        <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo_ua.jpeg" />
+                        <img src="<?php echo $CLIENT_ROOT; ?>/images/logo_ua.jpeg"/>
                 </a>
                 <a href="https://www.nationalgeographic.com/" target="_blank" title="National Geographic">
-                        <img src="<?php echo $CLIENT_ROOT; ?>/images/layout/logo-asu-biokic.png" />
+                        <img src="<?php echo $CLIENT_ROOT; ?>/images/logo-natgeo.svg"/>
                 </a>
         </div>
-        <p>This project made possible by the <a href="https://www.nsf.gov/">National Science Foundation</a> and <a href="https://www.nationalgeographic.com/">National Geographic Society</a>.</p>
+        <p>This project made possible by the <a href="https://www.nsf.gov/">National Science Foundation</a> and the <a href="https://www.nationalgeographic.com/">National Geographic Society</a>.</p>
         <p>For more information about Symbiota, <a href="https://symbiota.org/docs" target="_blank" rel="noopener noreferrer">read the docs</a> or contact the <a href="https://symbiota.org/contact-the-support-hub/" target="_blank" rel="noopener noreferrer">Symbiota Support Hub</a>.</p>
         </p>
         <p>Powered by <a href="https://symbiota.org/" target="_blank">Symbiota</a>.</p>
