2403ea84be6efa0ecdf96ab1edc004a752c1b07b
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-04-17 14:54:08 -0700

    Add title and subtitle

--- content/lang/header.en.php
+++ content/lang/header.en.php
@@ -6,6 +6,8 @@ Language: English
 */
 
 $LANG['H_HOME'] = 'Home';
+$LANG['H_TITLE'] = 'Guatemala Biodiversity Portal';
+$LANG['H_SUBTITLE']= 'Specimen data from Guatemala’s collections';
 
 $LANG['H_SEARCH'] = 'Search';
 $LANG['H_COLLECTIONS'] = 'Collections';
