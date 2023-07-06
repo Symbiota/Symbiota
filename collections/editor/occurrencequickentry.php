c47b84882e00b2f6ab2aa78f622d21459f3fe5a8
ttaliacc <taliac@bu.edu>
2023-07-06 08:54:53 +0000

    add the image zoom function

--- collections/editor/occurrencequickentry.php
+++ collections/editor/occurrencequickentry.php
@@ -57,7 +57,6 @@ $jumpStr = '';
 $isEditor = 0;
 
 // Dropdown arrays
-
 $filedUnderDrop = $occManager->getValues('dd_filedUnderID', 'dropdown_filedUnder_values');
 
 
@@ -544,6 +543,42 @@ else{
 		<script src="../../js/symb/collections.editor.imgtools.js?ver=2" type="text/javascript"></script>
 		<script src="../../js/jquery.imagetool-1.7.js?ver=140310" type="text/javascript"></script>
 		<script src="../../js/symb/collections.editor.query.js?ver=5" type="text/javascript"></script>
+		<script>
+			var zoomWindow;
+			function openZoomWindow(event) {
+				// Get the source image and its dimensions
+				var sourceImage = event.target;
+				var imgWidth = sourceImage.width;
+				var imgHeight = sourceImage.height;
+
+				// Calculate the click position relative to the image
+				var offsetX = event.offsetX;
+				var offsetY = event.offsetY;
+
+				// Calculate the ratio of click position to image size
+				var clickRatioX = offsetX / imgWidth;
+				var clickRatioY = offsetY / imgHeight;
+
+				// Calculate the position in the zoomed-in image
+				var zoomedImgWidth = imgWidth * 2;
+				var zoomedImgHeight = imgHeight * 2;
+				var zoomedPosX = (clickRatioX * zoomedImgWidth) - (imgWidth / 2);
+        		var zoomedPosY = (clickRatioY * zoomedImgHeight) - (imgHeight / 2);
+
+				// Open or focus the zoom window
+				if (zoomWindow && !zoomWindow.closed) {
+					zoomWindow.focus();
+				} else {
+					zoomWindow = window.open('', '_blank');
+				}
+
+				// Write the HTML content to the zoom window
+				zoomWindow.document.write('<html><head><title>Zoomed In Image</title></head><body style="text-align: center;">');
+				zoomWindow.document.write('<img src="' + sourceImage.src + '" style="width: ' + zoomedImgWidth + 'px; object-fit: none; object-position: -' + zoomedPosX + 'px -' + zoomedPosY + 'px;">');
+				zoomWindow.document.write('</body></html>');
+				zoomWindow.document.close();
+			}
+		</script>
 		<style type="text/css">
 			fieldset{ padding:15px }
 			fieldset > legend{ font-weight:bold; }
@@ -1076,7 +1111,7 @@ else{
 							<h2>Click to Zoom in Other Window</h2>
 						</div>
 						<div class="login-info" style = "backgroufnd-color:#86C5D8; text-align: center;">
-							<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOwxvSve1jvNKgXtY40U7wyrTGKuYGUcWCTg&usqp=CAU" />
+							<img id="activeimg-<?php echo $imgCnt; ?>" src="<?php echo $iUrl; ?>" style="width:300px" onclick="openZoomWindow(event)" />
 						</div>
 						<div class="login-info">
 							<!-- TODO: need to figure out how to deal with this input. It supposes to be generated automatically based on the form -->
@@ -1279,6 +1314,13 @@ else{
 						<?php echo "info needed" ?>
 					</div>
 				</section> 
+				<section id="imgtd" style="width:430px;" valign="top">
+					<?php
+					if($occId && ($fragArr || $specImgArr )){
+						include_once('includes/imgprocessor.php');
+					}
+					?>
+				</section>
 			</div>
 		</div>			
 	</div>
