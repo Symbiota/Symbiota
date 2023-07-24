40cab4819a47f8d10e0f2ef74c04e3695b529d34
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2023-07-24 05:04:54 -0400

    update changes to barcode dependency

--- config/schema-1.0/utf8/db_schema_patch-1.3.sql
+++ config/schema-1.0/utf8/db_schema_patch-1.3.sql
@@ -42,11 +42,20 @@ CREATE TABLE `batch_user` (
   CONSTRAINT `FK_batch_user_user` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Create table to hold barcode values for image
+DROP TABLE IF EXISTS `images_barcode`;
+CREATE TABLE `images_barcode` (
+  `imgid` int(10) unsigned NOT NULL,
+  `barcode` varchar(255) NOT NULL,
+  PRIMARY KEY (`barcode`),
+  KEY `FK_images_barcode_images` (`imgid`),
+  CONSTRAINT `FK_images_barcode_images` FOREIGN KEY (`imgid`) REFERENCES `images` (`imgid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 -- Add missing columns to the omocurrences table where the quick entry data is stored
 ALTER TABLE `omoccurrences` 
     ADD COLUMN `filedUnder` int DEFAULT NULL,
     ADD COLUMN `geoWithin` int DEFAULT NULL,
-    ADD COLUMN `barcode` int(8) NOT NULL,
     ADD COLUMN `herbarium` varchar(4) NOT NULL,
     ADD COLUMN `accesNum` varchar(255) DEFAULT NULL,
     ADD COLUMN `currName` int DEFAULT NULL,
