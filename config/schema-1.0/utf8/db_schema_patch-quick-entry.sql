78a6cd44b034f5dcc99e06bba7fa652ff7fa7135
Tian-Tan <101669234+Tian-Tan@users.noreply.github.com>
2023-07-25 02:34:49 -0400

    bugfix for previous commit

--- config/schema-1.0/utf8/db_schema_patch-quick-entry.sql
+++ config/schema-1.0/utf8/db_schema_patch-quick-entry.sql
@@ -642,5 +642,17 @@ CREATE TABLE `batch_user` (
   CONSTRAINT `FK_batch_user_user` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `images_barcode`;
+CREATE TABLE `images_barcode` (
+  `imgid` int(10) unsigned NOT NULL,
+  `barcode` varchar(255) NOT NULL,
+  `occid` int unsigned NOT NULL,
+  PRIMARY KEY (`barcode`),
+  KEY `FK_images_barcode_images` (`imgid`),
+  KEY `FK_images_barcode_omoccurrences` (`occid`),
+  CONSTRAINT `FK_images_barcode_images` FOREIGN KEY (`imgid`) REFERENCES `images` (`imgid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_images_barcode_omoccurrences` FOREIGN KEY (`occid`) REFERENCES `omoccurrences` (`occid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 ALTER TABLE `omoccurrences`
     DROP COLUMN `barcode`;
