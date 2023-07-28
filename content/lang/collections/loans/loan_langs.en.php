bd3d494c34ef95a71e992ea9422f373574ed1f44
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-07-28 14:15:58 -0700

    Lang tags for exchange.php

---
+++ content/lang/collections/loans/loan_langs.en.php
@@ -0,0 +1,56 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['EXCHANGE_MNG'] = 'Exchange Management';
+$LANG['COL_MNG_MENU'] = 'Collection Management Menu';
+$LANG['LOAN_INDEX'] = 'Loan Index';
+$LANG['EXCHANGE_DETS'] = 'Exchange Details';
+$LANG['ADMIN'] = 'Admin';
+$LANG['EDIT_ADJ'] = 'Edit Adjustment';
+$LANG['TRANS_NO'] = 'Transaction Number';
+$LANG['TRANS_TYPE'] = 'Transaction Type';
+$LANG['SHIPMENT'] = 'Shipment';
+$LANG['ADJUSTMENT'] = 'Adjustment';
+$LANG['ENTERED_BY'] = 'Entered By';
+$LANG['INSTITUTION'] = 'Institution';
+$LANG['ADJ_AMOUNT'] = 'Adjustment Amount';
+$LANG['EDIT_GIFT_EX'] = 'Edit Gift/Exchange';
+$LANG['DATE_SHIPPED'] = 'Date Shipped';
+$LANG['DATE_RECEIVED'] = 'Date Received';
+$LANG['INSTITUTION'] = 'Institution';
+$LANG['IN_OUT'] = 'In/Out';
+$LANG['OUT'] = 'Out';
+$LANG['IN'] = 'In';
+$LANG['GIFT_SPECIMENS'] = 'Gift Specimens';
+$LANG['EXCH_SPECIMENS'] = 'Exchange Specimens';
+$LANG['TRANS_TOTALS'] = 'Transaction Totals';
+$LANG['TOTAL_GIFTS'] = 'Total Gifts';
+$LANG['TOTAL_UNMOUNTED'] = 'Total Unmounted';
+$LANG['EXCHANGE_VALUE'] = 'Exchange Value';
+$LANG['TOTAL_GIFTS_DET'] = 'Total Gifts For Det';
+$LANG['TOTAL_MOUNTED'] = 'Total Mounted';
+$LANG['TOTAL_SPECIMENS'] = 'Total Specimen';
+$LANG['CURRENT_BALANCE'] = 'Current Balance';
+$LANG['NO_BOXES'] = '# of Boxes';
+$LANG['SHIPPING_SERVICE'] = 'Shipping Service';
+$LANG['DESCRIPTION'] = 'Description';
+$LANG['NOTES'] = 'Notes';
+$LANG['ADDITIONAL_MESSAGE'] = 'Additional Message';
+$LANG['SAVE'] = 'Save';
+$LANG['CORRESPONDENCE_ATTACH'] = 'Correspondence Attachments';
+$LANG['ADD_CORRESPONDENCE_ATTACH'] = 'Add Correspondence Attachment';
+$LANG['ATTACH_TITLE'] = 'Attachment Title';
+$LANG['SAVE_ATTACH'] = 'Save Attachment';
+$LANG['ATTACH_DESCRIPTION'] = 'Supported file types include PDF, Word, Excel, images (.jpg/.jpeg or png), and text files (.txt). </br>
+									PDFs, images, and text files are preferred, since they will display in the browser.';
+$LANG['RETURN_LOAN_INDEX'] = 'Return to Loan Index Page';
+$LANG['SURE_DELETE'] = 'Are you sure you want to permanently delete this exchange?';
+$LANG['DEL_EXCHANGE'] = 'Delete Exchange';
+$LANG['NOT_AUTHORIZED'] = 'You are not authorized to add occurrence records';
+$LANG['UNKNOWN_ERROR'] = 'ERROR: unknown error, please contact system administrator';
+
+?>
