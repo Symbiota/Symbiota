c0e37a8a3c41af14d22f25efe7f8fb07f82cd241
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-08-03 12:19:23 -0700

    Lang tags for exchangetab.php and incoming.php

--- content/lang/collections/loans/loan_langs.en.php
+++ content/lang/collections/loans/loan_langs.en.php
@@ -5,13 +5,14 @@ Language: English
 ------------------
 */
 
+// from exchange.php
 $LANG['EXCHANGE_MNG'] = 'Exchange Management';
 $LANG['COL_MNG_MENU'] = 'Collection Management Menu';
 $LANG['LOAN_INDEX'] = 'Loan Index';
 $LANG['EXCHANGE_DETS'] = 'Exchange Details';
 $LANG['ADMIN'] = 'Admin';
 $LANG['EDIT_ADJ'] = 'Edit Adjustment';
-$LANG['TRANS_NO'] = 'Transaction Number';
+$LANG['TRANS_NO'] = 'Transaction Number/Identifier';
 $LANG['TRANS_TYPE'] = 'Transaction Type';
 $LANG['SHIPMENT'] = 'Shipment';
 $LANG['ADJUSTMENT'] = 'Adjustment';
@@ -33,7 +34,7 @@ $LANG['TOTAL_UNMOUNTED'] = 'Total Unmounted';
 $LANG['EXCHANGE_VALUE'] = 'Exchange Value';
 $LANG['TOTAL_GIFTS_DET'] = 'Total Gifts For Det';
 $LANG['TOTAL_MOUNTED'] = 'Total Mounted';
-$LANG['TOTAL_SPECIMENS'] = 'Total Specimen';
+$LANG['TOTAL_SPECIMENS'] = 'Total Specimens';
 $LANG['CURRENT_BALANCE'] = 'Current Balance';
 $LANG['NO_BOXES'] = '# of Boxes';
 $LANG['SHIPPING_SERVICE'] = 'Shipping Service';
@@ -52,5 +53,50 @@ $LANG['SURE_DELETE'] = 'Are you sure you want to permanently delete this exchang
 $LANG['DEL_EXCHANGE'] = 'Delete Exchange';
 $LANG['NOT_AUTHORIZED'] = 'You are not authorized to add occurrence records';
 $LANG['UNKNOWN_ERROR'] = 'ERROR: unknown error, please contact system administrator';
+
+// from exchangetab.php
+$LANG['CREATE_NEW_EXCHANGE'] = 'Create New Exchange';
+$LANG['NEW_GIFT'] = 'New Gift/Exchange';
+$LANG['SEL_INST'] = 'Select Institution';
+$LANG['ADD_NEW_INST'] = 'Add a New Institution';
+$LANG['CREATE_EXCHANGE'] = 'Create Exchange';
+$LANG['TRANS_BY_INST'] = 'Transaction Records by Institution';
+$LANG['OUTGOING_EX_SENT'] = 'Outgoing exchange; Sent ';
+$LANG['INCLUDING'] = 'including';
+$LANG['INCOMING_EX_RECEIVED'] = 'Incoming exchange; Received ';
+$LANG['MOUNTED'] = 'mounted';
+$LANG['UNMOUNTED'] = 'unmounted';
+$LANG['GIFT'] = 'gift';
+$LANG['GIFT_FOR_DET'] = 'gift-for-det';
+$LANG['SPECIMENS'] = 'specimens';
+$LANG['NO_TRANSACTIONS'] = 'There are no transactions registered for this collection';
+
+// from incoming.php
+$LANG['INCOMING_LOAN_MANAGE'] = 'Incoming Loan Management';
+$LANG['DATE_EXAMPLE'] = 'Date (e.g. ';
+$LANG['VALUES_FORMAT'] = ') values must follow format: YYYY-MM-DD';
+$LANG['SEL_INSTITUTION'] = 'Select an institution';
+$LANG['ENTER_LOAN_NO'] = 'Enter the sender\'s loan number';
+$LANG['LOAN_DETAILS'] = 'Loan Details';
+$LANG['LOAN_IN_DETAILS'] = 'Loan In Details';
+$LANG['LOAN_NUMBER'] = 'Loan Number';
+$LANG['PROCESSED_BY'] = 'Processed By';
+$LANG['DATE_DUE'] = 'Date Due';
+$LANG['SENT_FROM'] = 'Sent From';
+$LANG['SENDERS_LOAN_NUMBER'] = 'Sender\'s Loan Number';
+$LANG['REQUESTED_FOR'] = 'Requested for';
+$LANG['LOAN_DESCRIPTION'] = 'Loan Description';
+$LANG['DATE_RETURNED'] = 'Date Returned';
+$LANG['RET_PROCESSED_BY'] = 'Ret. Processed By';
+$LANG['DATE_CLOSED'] = 'Date Closed';
+$LANG['ADD_INV_MESSAGE'] = 'Additional Invoice Message';
+$LANG['CATNO'] = 'Catalog Number';
+$LANG['DETAILS'] = 'Details';
+$LANG['OPEN_SPECIMEN_DETAILS'] = 'Open Specimen Details page';
+$LANG['OPEN_OCC_EDITOR'] = 'Open Occurrence Editor';
+$LANG['SURE_DEL_LOAN'] = 'Are you sure you want to permanently delete this loan?';
+$LANG['DEL_INC_LOAN'] = 'Delete Incoming Loan';
+$LANG['REMOVE_SPECIMENS_TO_DEL'] = 'Loan cannot be deleted until all linked specimens are removed (can only be done by lending institution)';
+$LANG['DELETE_LOAN'] = 'Delete Loan';
 
 ?>
