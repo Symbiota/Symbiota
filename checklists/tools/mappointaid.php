51b6584c0b40a75bd7d6312b8b0e058abd097784
MuchQuak <loganpwilt@gmail.com>
2023-08-17 11:28:18 -0700

    more html fixes

--- checklists/tools/mappointaid.php
+++ checklists/tools/mappointaid.php
@@ -29,12 +29,12 @@ else{
 }
 ?>
 <html>
-	<head>
-		<title><?php echo $DEFAULT_TITLE; ?> - Coordinate Aid</title>
-		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+   <head>
+      <title><?php echo $DEFAULT_TITLE; ?> - Coordinate Aid</title>
+      <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 
-	   <?php include_once($SERVER_ROOT.'/includes/leafletMap.php')?>
-		<script src="//maps.googleapis.com/maps/api/js?<?php echo (isset($GOOGLE_MAP_KEY) && $GOOGLE_MAP_KEY?'key='.$GOOGLE_MAP_KEY:''); ?>"></script>
+      <?php include_once($SERVER_ROOT.'/includes/leafletMap.php')?>
+      <script src="//maps.googleapis.com/maps/api/js?<?php echo (isset($GOOGLE_MAP_KEY) && $GOOGLE_MAP_KEY?'key='.$GOOGLE_MAP_KEY:''); ?>"></script>
       <script type="text/javascript">
       var map;
       var currentMarker;
