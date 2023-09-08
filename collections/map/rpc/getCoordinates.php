23ace5e8cf43e495defe35a501544bca2da7440e
MuchQuak <loganpwilt@gmail.com>
2023-09-07 19:56:50 -0700

    Added json headers to getTaxa and getCoordinates

--- collections/map/rpc/getCoordinates.php
+++ collections/map/rpc/getCoordinates.php
@@ -2,6 +2,8 @@
 include_once('../../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/MapSupport.php');
 
+header('Content-Type: application/json');
+
 $tid = $_REQUEST['tid'];
 $bounds = $_REQUEST['bounds'];
 
@@ -11,5 +13,6 @@ if($IS_ADMIN && is_numeric($tid)){
 	//$tid = $mapManager->sanitizeInt($tid);
 	$retArr = $mapManager->getCoordinates($tid, $bounds);
 }
+
 echo json_encode($retArr);
 ?>
--- collections/map/rpc/getTaxa.php
+++ collections/map/rpc/getTaxa.php
@@ -2,6 +2,8 @@
 include_once('../../../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/MapSupport.php');
 
+header('Content-Type: application/json');
+
 $retArr = false;
 if($IS_ADMIN){
 	$mapManager = new MapSupport();
