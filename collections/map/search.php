f2539d68dcef59b989bec79236bdeaba260499a9
MuchQuak <loganpwilt@gmail.com>
2023-09-18 12:37:26 -0700

    Some comments for my thoughts

--- collections/map/search.php
+++ collections/map/search.php
@@ -1,3 +1,8 @@
 <?php
 echo "Hello Search"
+// Container -> Set Coordinates on with some standard typing 
+// ex: Marker { lat, lng, popup, tooltip , MarkerType } obs vs spec
+//
+// Base Map Class
 ?>
+
