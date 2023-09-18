f0bddd0045314db2fb04a37c81aff1f62c8192cd
Nikita Salikov <nsalikov@asu.edu>
2023-09-18 11:07:10 -0700

    508 fixes, added labels, added script for onchange, removed form submit function return, internationalized text

---
+++ js/symb/ident.matrixeditor.js
@@ -0,0 +1,5 @@
+document.getElementById("selection").addEventListener("change", function() {
+    document.getElementById("filterform").submit(); // Submit the form when an option is selected
+});
+
+
