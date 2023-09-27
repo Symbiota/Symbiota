9ad54ca7168bffebbd296a268553b2a4f1e4c137
Edward Gilbert <egbot@asu.edu>
2023-09-27 14:08:37 -0700

    EcoFlora Dev
    - minor adjustments

--- config/schema/3.0/patches/db_schema_patch-3.1.sql
+++ config/schema/3.0/patches/db_schema_patch-3.1.sql
@@ -11,8 +11,8 @@ ALTER TABLE `fmchklstcoordinates`
 
 # Needed to ensure basisOfRecord values are tagged correctly based on collection type (aka collType field)
 UPDATE omoccurrences o INNER JOIN omcollections c ON o.collid = c.collid
-SET o.basisofrecord = "PreservedSpecimen"
-WHERE (o.basisofrecord = "HumanObservation" OR o.basisofrecord IS NULL) AND c.colltype = 'Preserved Specimens'
-AND o.occid NOT IN(SELECT occid FROM omoccuredits WHERE fieldname = "basisofrecord");
+  SET o.basisofrecord = "PreservedSpecimen"
+  WHERE (o.basisofrecord = "HumanObservation" OR o.basisofrecord IS NULL) AND c.colltype = 'Preserved Specimens'
+  AND o.occid NOT IN(SELECT occid FROM omoccuredits WHERE fieldname = "basisofrecord");
 
 
