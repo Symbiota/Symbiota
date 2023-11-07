84de144516dfb22393089a031a3a0107754e9b60
Mark <mark.aaron.fisher@gmail.com>
2023-11-07 15:30:55 -0800

    fix spacing

--- collections/cleaning/politicalunits.php
+++ collections/cleaning/politicalunits.php
@@ -158,11 +158,11 @@ if($action && $isEditor){
 			<hr/>
 			<?php
 		}
-		echo '<h2>'.$collMap['collectionname'].' ('.$collMap['code'].')</h2>';
+		echo '<h2>' . $collMap['collectionname'] . ' (' . $collMap['code'] . ')</h2>';
 		if($isEditor){
 			?>
 			<section class="fieldset-like">
-				<h1> <span> <?php echo (isset($LANG['GEO_REPORT'])?$LANG['GEO_REPORT']:'Geographic Report'); ?> </span> </h1>
+				<h1> <span> <?php echo (isset($LANG['GEO_REPORT']) ? $LANG['GEO_REPORT'] : 'Geographic Report'); ?> </span> </h1>
 				<?php
 				if($target == 'geolocal'){
 					if($mode) echo '<div style="position:absolute;top:5px;right:0px;padding:10px;border:1px solid grey"><a href="politicalunits.php?collid=' . htmlspecialchars($collid, HTML_SPECIAL_CHARS_FLAGS) . '&mode=0">' . htmlspecialchars($LANG['MAIN_MENU'], HTML_SPECIAL_CHARS_FLAGS) . '</a></div>';
