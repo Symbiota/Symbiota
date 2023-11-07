545b386fd1eb432c7e491377896b75033bb6becb
MuchQuak <loganpwilt@gmail.com>
2023-11-07 14:20:33 -0800

    adding a unqiue id guard for mulitple imports and on change value switches to turn on portal selector and track selected portal

--- collections/map/portalSelector.php
+++ collections/map/portalSelector.php
@@ -8,33 +8,55 @@ $conn = MySQLiConnectionFactory::getCon('readonly');
 $portals = $conn->query(<<<sql
 SELECT portalName, urlRoot from portalindex p;
 sql)->fetch_all(MYSQLI_ASSOC);
-//$portals[0]['urlRoot'] . 
+
+//Kinda a getto way of ensuring unique id's if multiple of this file is
+//included. 
+$PORTAL_SELECTOR_ID = !isset($PORTAL_SELECTOR_ID) || !is_int($PORTAL_SELECTOR_ID)? 0: $PORTAL_SELECTOR_ID + 1;
+
 ?>
 <div>
    <script src="<?php echo $CLIENT_ROOT?>/js/autocomplete-input.js" type="module"></script>
+   <script type="text/javascript">
+   function onPortalSelect(v) {
+      let input = document.getElementById("portal-taxa-suggest-<?php echo $PORTAL_SELECTOR_ID?>")
+      input.completeUrl = v + '/rpc/taxasuggest.php?term=??';
+   }
+
+   function onEnablePortalSelector(on) {
+      let selector = document.getElementById("portal-selector-<?php echo $PORTAL_SELECTOR_ID?>")
+      selector.style.display= on ?'block': 'none';
+   }
+   </script>
    <div>
-      <input data_role="none" type="checkbox" id="cross_portal_switch" name="cross_portal_switch"/>
+      <input 
+         onchange="onEnablePortalSelector(this.checked)"
+         data_role="none" 
+         type="checkbox" 
+         name="cross_portal_switch"/>
       <label for="cross_portal_switch">
          <?php echo (isset($LANG['ENABLE_CROSS_PORTAL_SEARCH'])? $LANG['ENABLE_CROSS_PORTAL_SEARCH']: 'Enable Cross Portal Search')?>
       </label>
    </div>
    <br/>
-   <div>   
-      <label>Portal:</label>
-      <select onchange="onPortalSelect(this.value)">
-         <?php foreach($portals as $portal): ?>
-         <option value="<?= $portal['urlRoot']?>"><?=$portal['portalName']?></option>
-         <?php endforeach; ?>
-      </select>
-   </div>
-   <br/>
-   <div>
-      <label>Taxa:</label>
-      <autocomplete-input 
-         response_type="json"
-         json_label="value"
-         json_value="id"
-         completeUrl="<?= '/Portal/rpc/taxasuggest.php?term=??'?>">
-      </autocomplete-input>
+   <div id="portal-selector-<?php echo $PORTAL_SELECTOR_ID?>" style="display:none">
+      <div>   
+         <label>Portal:</label>
+         <select value="Choose a Portal to Search" onchange="onPortalSelect(this.value)">
+            <?php foreach($portals as $portal): ?>
+            <option value="<?= $portal['urlRoot']?>"><?=$portal['portalName']?></option>
+            <?php endforeach; ?>
+         </select>
+      </div>
+      <br/>
+      <div>
+         <label>Taxa:</label>
+         <autocomplete-input 
+            id="portal-taxa-suggest-<?php echo $PORTAL_SELECTOR_ID?>"
+            response_type="json"
+            json_label="value"
+            json_value="id"
+            completeUrl="<?= '/Portal/rpc/taxasuggest.php?term=??'?>">
+         </autocomplete-input>
+      </div>
    </div>
 </div>
