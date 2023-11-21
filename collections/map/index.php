f40801a2670af9b3e874a4bb17135ea55b386e2c
MuchQuak <loganpwilt@gmail.com>
2023-11-21 15:45:01 -0800

    add verify form to submit listeners for google and leaflet

--- collections/map/index.php
+++ collections/map/index.php
@@ -748,6 +748,7 @@ value="${color}"
 
 
 			document.getElementById("mapsearchform").addEventListener('submit', async e => {
+				if(!verifyCollForm(e.target)) return;
 				showWorking();
 				e.preventDefault();
 				let formData = new FormData(e.target);
@@ -1157,6 +1158,8 @@ value="${color}"
 			}
 
 			document.getElementById("mapsearchform").addEventListener('submit', async e => {
+				if(!verifyCollForm(e.target)) return;
+
 				showWorking();
 				e.preventDefault();
 				let formData = new FormData(e.target);
@@ -1543,11 +1546,11 @@ value="${color}"
 				alert("Failed to initialize map coordinate data")
 			}
 
-			<?php if(empty($GOOGLE_MAP_KEY)) { ?> 
+			<?php if(empty($GOOGLE_MAP_KEY)): ?> 
 				leafletInit();
-			<?php } else { ?> 
-			googleInit();
-		<?php } ?>
+			<?php else: ?> 
+				googleInit();
+			<?php endif?>
 	  }
 		</script>
 		<script src="../../js/symb/api.taxonomy.taxasuggest.js?ver=4" type="text/javascript"></script>
@@ -1578,7 +1581,7 @@ value="${color}"
 					<div id="accordion">
 						<h3 style="padding-left:30px;"><?php echo (isset($LANG['SEARCH_CRITERIA'])?$LANG['SEARCH_CRITERIA']:'Search Criteria and Options'); ?></h3>
 						<div id="tabs1" style="width:379px;padding:0px;height:100%">
-							<form name="mapsearchform" id="mapsearchform" data-ajax="false" action="search.php" method="post" onsubmit="return verifyCollForm(this);">
+							<form name="mapsearchform" id="mapsearchform" data-ajax="false">
 								<ul>
 									<li><a href="#searchcollections"><span><?php echo htmlspecialchars((isset($LANG['COLLECTIONS'])?$LANG['COLLECTIONS']:'Collections'), HTML_SPECIAL_CHARS_FLAGS); ?></span></a></li>
 									<li><a href="#searchcriteria"><span><?php echo htmlspecialchars((isset($LANG['CRITERIA'])?$LANG['CRITERIA']:'Criteria'), HTML_SPECIAL_CHARS_FLAGS); ?></span></a></li>
