c3105a2362fcee70efe916f005f58e52bf4e65e2
Mark <mark.aaron.fisher@gmail.com>
2023-12-07 13:24:37 -0800

    add off-screen label for checkbox in table

--- collections/editor/editreviewer.php
+++ collections/editor/editreviewer.php
@@ -357,9 +357,9 @@ $navStr .= '</div>';
 					echo '<div style="clear:both">'.$navStr.'</div>';
 					?>
 					<table class="styledtable" style="font-family:Arial;font-size:1.25rem;" aria-label="<?php echo (isset($LANG['TABLE']) ? $LANG['TABLE'] : 'Table of Records'); ?>" aria-describedby="table-desc">
-						<p id="table-desc">
+						<caption id="table-desc" class="bottom-breathing-room-relative top-breathing-room-rel">
 							<?php echo (isset($LANG['TABLE_DESC']) ? $LANG['TABLE_DESC'] : 'The table contains Record Id, Catalog Number, Review Status, Applied Status, Editor Name, Timestamp, Field Name, Old Value, and New Value for each entry'); ?>
-						</p>
+						</caption>
 						<tr>
 							<th> <input name='selectall' type="checkbox" onclick="selectAllId(this)" aria-label="<?php echo (isset($LANG['SELECT_ALL']) ? $LANG['SELECT_ALL'] : 'Select/Unselect All'); ?>" /></th>
 							<th><?php echo $LANG['RECORD_NO']; ?></th>
@@ -389,7 +389,8 @@ $navStr .= '</div>';
 												<td>
 													<?php
 													if($displayAll){
-														echo '<input name="id[]" type="checkbox" value="'.$id.'" />';
+														echo '<input id="id[]-' . $id . '" name="id[]" type="checkbox" value="'.$id.'" />';
+														echo '<label class="skip-link" for="id[]-' . $id . '">' . $id . '</label>';
 													}
 													?>
 												</td>
