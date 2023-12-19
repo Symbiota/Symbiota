f19ce6c5b483e6639e5c0940818eeab7c6a7a448
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-12-19 09:30:47 -0800

    Add spaces

--- checklists/dynamicchecklist.php
+++ checklists/dynamicchecklist.php
@@ -1,7 +1,7 @@
 <?php
 include_once('../config/symbini.php');
-include_once($SERVER_ROOT.'/classes/DynamicChecklistManager.php');
-include_once($SERVER_ROOT.'/content/lang/checklists/checklist.'.$LANG_TAG.'.php');
+include_once($SERVER_ROOT . '/classes/DynamicChecklistManager.php');
+include_once($SERVER_ROOT . '/content/lang/checklists/checklist . ' . $LANG_TAG . '.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 
 $lat = $_POST['lat'];
