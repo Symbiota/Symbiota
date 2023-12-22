f380e8af3e13522864fb960c64b48c8add42f9c6
Katie Pearson <katelin.d.pearson24@gmail.com>
2023-12-22 11:25:26 -0800

    Icon styling and adjustment to lang tag to reference new icon

--- collections/editor/includes/imagetab.php
+++ collections/editor/includes/imagetab.php
@@ -57,7 +57,7 @@ $photographerArr = $occManager->getPhotographerArr();
 </script>
 <div id="imagediv" style="width:795px;">
 	<div style="float:right;cursor:pointer;" onclick="toggle('addimgdiv');" title="<?php echo $LANG['ADD_IMG']; ?>">
-		<img style="border:0px;width:12px;" src="../../images/add.png" />
+		<img style="border:0px;width:1.5em;" src="../../images/add.png" />
 	</div>
 	<div id="addimgdiv" style="display:<?php echo ($specImgArr?'none':''); ?>;">
 		<form name="imgnewform" action="occurrenceeditor.php" method="post" enctype="multipart/form-data" onsubmit="return verifyImgAddForm(this);">
@@ -211,7 +211,7 @@ $photographerArr = $occManager->getPhotographerArr();
 						</td>
 						<td style="text-align:left;padding:10px;">
 							<div style="float:right;cursor:pointer;" onclick="toggle('img<?php echo $imgId; ?>editdiv');" title="<?php echo $LANG['EDIT_METADATA']; ?>">
-								<img style="border:0px;width:12px;" src="../../images/edit.png" />
+								<img style="border:0px;width:1.2em;" src="../../images/edit.png" />
 							</div>
 							<div style="margin-top:30px">
 								<div>
