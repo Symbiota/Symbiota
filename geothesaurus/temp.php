57bdf9ec668c95bf88962c8ad0eebb96f4052f30
MuchQuak <loganpwilt@gmail.com>
2024-01-17 15:43:17 -0800

    removing temp

--- geothesaurus/temp.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once ('../config/symbini.php');
-include_once ($SERVER_ROOT . '/classes/GeographicThesaurus.php');
-
-$geoManager = new GeographicThesaurus();
-
-//echo  $geoManager->getGeoThesIDV2("American Samoa", "ADM1", 223);
-//echo  $geoManager->getGeoThesIDV2("American Samoa", "ADM1");
-?>
