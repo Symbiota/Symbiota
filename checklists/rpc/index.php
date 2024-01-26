bc61eda2df1991f34b9664abb56e4d7e8385274b
Nikita Salikov <nsalikov@asu.edu>
2024-01-26 13:15:06 -0700

    lang-checklists-rpc-index

--- checklists/rpc/index.php
+++ checklists/rpc/index.php
@@ -1,9 +1,9 @@
 <?php
 include_once('../../config/symbini.php');
-if($LANG_TAG != 'en' && file_exists($SERVER_ROOT.'/content/lang/checklists//alerts.'.$LANG_TAG.'.php'))
-	include_once($SERVER_ROOT.'/content/lang/checklists//alerts.'.$LANG_TAG.'.php');
+if($LANG_TAG != 'en' && file_exists($SERVER_ROOT.'/content/lang/checklists/alerts.'.$LANG_TAG.'.php'))
+	include_once($SERVER_ROOT.'/content/lang/checklists/alerts.'.$LANG_TAG.'.php');
 else
-	include_once($SERVER_ROOT.'/content/lang/checklists//alerts.en.php');
+	include_once($SERVER_ROOT.'/content/lang/checklists/alerts.en.php');
 header('Content-Type: text/html; charset=' . $CHARSET);
 header('Location: '.$CLIENT_ROOT.'/index.php');
 ?>
