3db905b6be15a79ab83dda235f1fe0711c38d8de
Nikita Salikov <nsalikov@asu.edu>
2024-01-26 13:02:31 -0700

    Internationalized text, added lang files

---
+++ content/lang/checklists/checklistloader.en.php
@@ -0,0 +1,41 @@
+<?php
+/*
+------------------
+Language: English
+------------------
+*/
+
+$LANG['SELECT_FILE'] = 'Please select a file to upload';
+$LANG['DOCUMENT'] = 'Document ';
+$LANG['MUST_BE_CSV'] = 'must be a CSV file (with a .csv extension) ';
+$LANG['RETURN_CHECKLIST'] = 'Return to Checklist';
+$LANG['CHECK_LOADER'] = 'Checklists Loader';
+$LANG['RETURN_LOADER'] = 'Return to Loader';
+$LANG['INPUT_MATCH'] = 'and make sure the input file matches requirements within instructions';
+$LANG['UPLOAD_STATUS'] = 'Upload status...';
+$LANG['TAXA_LOADED'] = 'Taxa successfully loaded:';
+$LANG['PROBLEM_TAXA'] = 'Problematic Taxa:';
+$LANG['GENERAL_ERRORS'] = 'General errors:';
+$LANG['UPLOAD_COMPLETE'] = 'Upload Complete!';
+$LANG['TAXA_RESOLUTION'] = 'Problematic Taxa Resolution';
+$LANG['GENERAL_ERRORS'] = 'general errors';
+$LANG['UPLOAD_FORM'] = 'Checklist Upload Form';
+$LANG['CHECKLIST_FILE'] = 'Checklist File:';
+$LANG['TAXA_RESOLUTION'] = 'Taxonomic Resolution:';
+$LANG['LEAVE_TAXA'] = 'Leave Taxonomy As Is';
+$LANG['FILE_DESCR'] = 'Input file must be a CSV text file containing the following columns.
+            Column order does not matter, though the first row should contain columns names in accordance with the names in bold listed below.
+            Note that Excel spreadsheets (xlsx) can be saved as a CSV file via the "Save as..." option.';
+$LANG['SCINAME'] = 'sciname';
+$LANG['FAMILY'] = 'family';
+$LANG['HABITAT'] = 'habitat';
+$LANG['ABUNDANCE'] = 'abundance';
+$LANG['NOTES'] = 'notes';
+$LANG['INTERNALNOTES'] = 'internalnotes';
+$LANG['SOURCE'] = 'source';
+$LANG['REQUIRED'] = '(required)';
+$LANG['OPTIONAL'] = '(optional)';
+$LANG['OPTIONAL_DISP'] = '(optional) - displayed only to editors';
+$LANG['NO_RIGHTS'] = 'You appear not to have rights to edit this checklist. If you think this is in error, contact an administrator';
+
+?>
