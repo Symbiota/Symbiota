4f4f0ba58679cd477e10c5f1800541bd2c5d006b
Nikita Salikov <nsalikov@asu.edu>
2024-01-31 11:51:50 -0700

    removed version checker and associated lang files, removed extra vManager assign to null

--- checklists/voucherreporthandler.php
+++ checklists/voucherreporthandler.php
@@ -1,21 +1,11 @@
 <?php
 include_once('../config/symbini.php');
 include_once($SERVER_ROOT.'/classes/ChecklistVoucherPensoft.php');
-if($LANG_TAG != 'en' && file_exists($SERVER_ROOT.'/content/lang/checklists/alerts.'.$LANG_TAG.'.php'))
-	include_once($SERVER_ROOT.'/content/lang/checklists/alerts.'.$LANG_TAG.'.php');
-else
-	include_once($SERVER_ROOT.'/content/lang/checklists/alerts.en.php');
-
 
 $clid = $_REQUEST['clid'];
 $rType = $_REQUEST['rtype'];
 
 if($rType == 'pensoftxlsx'){
-	$vManager = null;
-	if(version_compare(phpversion(), '5.6', '<')) {
-		echo $LANG['ERROR_EXCEL'];
-		exit;
-	}
 	$vManager = new ChecklistVoucherPensoft();
 	$vManager->setClid($clid);
 	$vManager->setCollectionVariables();
