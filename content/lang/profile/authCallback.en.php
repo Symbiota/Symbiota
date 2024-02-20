82931ae8e6139c9dc82b153fafccebc0e54aa2d3
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-02-20 12:00:14 -0700

    Removed the Location tag

--- content/lang/profile/authCallback.en.php
+++ content/lang/profile/authCallback.en.php
@@ -6,8 +6,7 @@ Language: English
 */
 
 $LANG['CAUGHT_EXCEPTION'] = 'Caught exception:';
-$LANG['LOCATION'] = 'Location:';
-$LANG['UNKNOWN_ERROR'] = 'Unkown Error - Could not authenticate - try again later or alert a system admin';
+$LANG['UNKNOWN_ERROR'] = 'Unknown Error - Could not authenticate - try again later or alert a system admin';
 $LANG['ERROR'] = 'Error - Could not authenticate with Authentication provider';
 $LANG['UNABLE_RETRIEVE_EMAIL'] = 'Unable to retrieve email address from authentication provider.';
 $LANG['AUTHENTICATION_FAILED'] = 'Authentication failed.';
