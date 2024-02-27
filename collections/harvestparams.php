9b2f4df45384134b6fca9694ede88c3205911930
Edward Gilbert <egbot@asu.edu>
2024-02-27 08:20:40 -0700

    Internationalization adjustments

--- collections/harvestparams.php
+++ collections/harvestparams.php
@@ -283,7 +283,7 @@ $SHOULD_INCLUDE_CULTIVATED_AS_DEFAULT = $SHOULD_INCLUDE_CULTIVATED_AS_DEFAULT ??
 						<label for="materialsampletype"><?= $LANG['MATERIAL_SAMPLE_TYPE'] ?></label>
 						<select name="materialsampletype" id="materialsampletype">
 							<option value="">---------------</option>
-							<option value="all-ms">All Records with Material Samples</option>
+							<option value="all-ms"><?= $LANG['ALL_MATERIAL_SAMPLE'] ?></option>
 							<?php
 							foreach($matSampleTypeArr as $matSampeType){
 								echo '<option value="' . $matSampeType . '">' . $matSampeType . '</option>';
