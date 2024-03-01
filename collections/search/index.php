89b56a1b56935ddde680a90edaad237a356cfcc2
Mark <mark.aaron.fisher@gmail.com>
2024-03-01 13:21:54 -0800

    bug fix the missing id attribute for the materialsampletypes

--- collections/search/index.php
+++ collections/search/index.php
@@ -442,7 +442,7 @@ $obsArr = (isset($collList['obs'])?$collList['obs']:null);
 										<option id="materialsampletype-all-ms" data-chip="<?php echo $LANG['MATERIAL_SAMPLE'] . ': ' . $LANG['ALL_MATERIAL_SAMPLE'] ?>" value="all-ms"><?= $LANG['ALL_MATERIAL_SAMPLE'] ?></option>
 										<?php
 										foreach($matSampleTypeArr as $matSampeType){
-											echo '<option data-chip=' . $LANG['MATERIAL_SAMPLE'] . ': ' . $matSampeType . 'value="' . $matSampeType . '">' . $matSampeType . '</option>';
+											echo '<option id="materialsampletype-' . $matSampeType . '" data-chip="' . $LANG['MATERIAL_SAMPLE'] . ': ' . $matSampeType . '" value="' . $matSampeType . '">' . $matSampeType . '</option>';
 										}
 										?>
 									</select>
