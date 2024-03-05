13982c398abe44be59075a2b31488b915ff7ca68
MuchQuak <loganpwilt@gmail.com>
2024-03-05 11:52:13 -0800

    fixing other translations

--- content/lang/geothesaurus/harvester.en.php
+++ content/lang/geothesaurus/harvester.en.php
@@ -4,13 +4,14 @@
 Language: English
 ------------------
 */
+
 $LANG['GEOGRAPHIC_HARVESTER'] = 'Geographic Harvester';
 $LANG['LOOKUP_TABLES_TITLE'] = 'Geopraphic Lookup Tables - deprecated';
 $LANG['LOOKUP_TABLES_DESC'] = 'There appears to be records within the deprecated Geographic lookup tables that are no longer used.<br/>Do you want to transfer this data into the new geographic thesaurus?';
 $LANG['TRANSFER_LOOKUP_TABLES'] = 'Transfer Lookup Tables';
 $LANG['ACTIVE_GEOGRAPHIC_THESAURUS'] = 'Active Geographic Thesaurus';
 $LANG['SHOW_ALL']  = 'Show all boundaries';
-$LANG['SHOW_NO_POLYGON']  = 'Show boundaries missing polygon';
+$LANG['SHOW_NO_POLYGON']  = 'Show boundaries missing a polygon';
 $LANG['SHOW_NO_DATABASE']  = 'Show boundaries missing in database';
 $LANG['COUNTRY'] = 'Country';
 $LANG['COUNTY'] = 'County';
@@ -37,49 +38,3 @@ $LANG['TABLE_LICENSE'] = 'License';
 $LANG['TABLE_REGION'] = 'Region';
 $LANG['TABLE_FULL_LINK'] = 'Full Link';
 $LANG['TABLE_IMAGE_PREVIEW'] = 'Image Preview';
-/*
-$LANG['GEOTHES_TITLE'] = 'Geographic Thesaurus Manager';
-$LANG['NAV_HOME'] = 'Home';
-$LANG['NAV_GEOTHES'] = 'Geographic Thesaurus Root Terms';
-$LANG['NAVIGATION_PANEL'] = 'Navigation Panel';
-$LANG['SHOW_BASE_LIST'] = 'Show Root Node';
-$LANG['SHOW_PARENT_NODE'] = 'Show Parent Node';
-$LANG['SHOW_PARENT'] = 'Show Parent';
-$LANG['SHOW_CHILDREN'] = 'Show children';
-$LANG['GOTO_HARVESTER'] = 'Go to Geographic Harvester';
-$LANG['EDIT'] = 'Edit';
-$LANG['GEO_UNIT'] = 'Geographic Unit';
-$LANG['GEO_UNIT_NAME'] = 'GeoUnit Name';
-$LANG['ABBR'] = 'Abbreviation';
-$LANG['ISO2'] = 'ISO2 Code';
-$LANG['ISO3'] = 'ISO3 Code';
-$LANG['NUM_CODE'] = 'Numeric Code';
-$LANG['GEO_RANK'] = 'Geography Rank';
-$LANG['SELECT_RANK'] = 'Select Rank';
-$LANG['NOTES'] = 'Notes';
-$LANG['PARENT_TERM'] = 'Parent term';
-$LANG['IS_ROOT_TERM'] = 'Is a Root Term (i.e. no parent)';
-$LANG['ACCEPTED_TERM'] = 'Preferred term';
-$LANG['IS_ACCEPTED'] = 'Term is Preferred';
-$LANG['SAVE_EDITS'] = 'Save Edits';
-$LANG['DEL_GEO_UNIT'] = 'Delete Geographic Unit';
-$LANG['CONFIRM_DELETE'] = 'Are you sure you want to delete this record?';
-$LANG['CANT_DELETE'] = 'Record can not be deleted until all child records are deleted';
-$LANG['SHOW'] = 'Show';$LANG['TERMS'] = 'terms';
-$LANG['ADD_GEO_UNIT'] = 'Add Geographic Unit';
-$LANG['SELECT_PARENT'] = 'Select Parent Term';
-$LANG['SELECT_ACCEPTED'] = 'Select Preferred Term';
-$LANG['ADD_UNIT'] = 'Add Unit';
-$LANG['TERMS_WITHIN'] = 'geographic terms within';
-$LANG['ROOT_TERMS'] = 'Root Terms (terms without parents)';
-$LANG['CHILDREN'] = 'children';
-$LANG['NO_RECORDS'] = 'No records returned';
-$LANG['POLYGON'] = 'Polygon';
-$LANG['NO_POLYGON'] = 'No';
-$LANG['YES_POLYGON'] = 'Yes';
-$LANG['EDIT_POLYGON'] = 'Edit Polygon';
-$LANG['SEARCH'] = 'Search';
-$LANG['OTHER'] = 'Other ';
-$LANG['SEARCH_GEOTHESAURUS'] = 'Search Geographic Thesaurus';
-*/
-?>
