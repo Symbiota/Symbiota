8ec19f9fe6f90a96b16a0ac048bb627de6230eaa
Mark <mark.aaron.fisher@gmail.com>
2024-03-11 19:43:16 -0700

    respond to code review feedback

--- config/schema/3.0/dev/cultivated_species_security_fix.sql
+++ config/schema/3.0/dev/cultivated_species_security_fix.sql
@@ -1,4 +1,9 @@
 # Clean up localitySecurity for occurrences that are cultivated and have not explicitly had their localitySecurity edited to be 1
 
-UPDATE omoccurrences SET localitySecurityReason = 'a user explicitly set localitySecurity on this occurrence' WHERE occid IN (SELECT occid FROM omoccuredits WHERE fieldName='localitySecurity' AND fieldValueNew=1);
+UPDATE omoccurrences o INNER JOIN omoccuredits e ON o.occid = e.occid
+LEFT JOIN (SELECT occid, ocedid FROM omoccuredits WHERE fieldName = "localitySecurity" AND fieldValueNew = 0) e2 ON e.occid = e2.occid AND e.ocedid < e2.ocedid
+SET o.localitySecurity = 1, o.localitySecurityReason = "[Security Setting Explicitly Locked]"
+WHERE o.localitySecurityReason IS NULL AND e.fieldName = "localitySecurity" AND e.fieldValueNew = 1
+AND e2.occid IS NULL;
+
 UPDATE omoccurrences SET localitySecurity=0 WHERE occid IN (SELECT occid FROM omoccurrences WHERE cultivationStatus=1 AND localitySecurity=1 AND localitySecurityReason IS NULL);
