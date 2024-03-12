982aa9b90c184ddc59184b41c126e832d7cb4c4b
Ramisa Zaman <129628108+RamisaZaman@users.noreply.github.com>
2024-03-12 11:14:53 -0700

    Fixed the issues from the tvp-v2

--- collections/loans/index.php
+++ collections/loans/index.php
@@ -240,7 +240,7 @@ if($isEditor){
 						?>
 						<div id="loanoutToggle" style="float:right;margin:10px;">
 							<a href="#" onclick="displayNewLoanOut();">
-								<img src="../../images/add.png" style="width:1.5em;" title="<?php echo $LANG['CREATE_NEW_LOAN']; ?>" alt="<?php echo $LANG['CREATE_NEW_LOAN']; ?>"  />
+								<img src="../../images/add.png" style="width:1.5em;" alt="<?php echo $LANG['CREATE_NEW_LOAN']; ?>"  />
 							</a>
 						</div>
 						<?php
@@ -280,7 +280,7 @@ if($isEditor){
 										</select>
 									</span>
 									<span>
-										<a href="../misc/institutioneditor.php?emode=1" target="_blank" title="<?php echo $LANG['ADD_NEW_INST']; ?>">
+										<a href="../misc/institutioneditor.php?emode=1" target="_blank" aria-label="<?php echo $LANG['ADD_NEW_INST']; ?>">
 											<img src="../../images/add.png" style="width:1.2em;" alt="<?php echo $LANG['CREATE_NEW_LOAN']; ?>" />
 										</a>
 									</span>
@@ -362,7 +362,7 @@ if($isEditor){
 					?>
 					<div id="loaninToggle" style="float:right;margin:10px;">
 						<a href="#" onclick="displayNewLoanIn();">
-							<img src="../../images/add.png" style="width:1.5em;" title="<?php echo $LANG['CREATE_NEW_LOAN']; ?>" alt="<?php echo $LANG['CREATE_NEW_LOAN']; ?>" />
+							<img src="../../images/add.png" style="width:1.5em;" alt="<?php echo $LANG['CREATE_NEW_LOAN']; ?>" />
 						</a>
 					</div>
 					<div id="newloanindiv" style="display:<?php echo (($loanInList || $loansOnWay || $searchTerm)?'none':'block'); ?>;">
@@ -380,7 +380,7 @@ if($isEditor){
 								<div style="padding-top:15px;float:right;">
 									<span>
 										<b><label for="loanidentifierborr"><?php echo $LANG['LOAN_ID']; ?></label>: </b>
-										<input type="text" id="loanidentifierborr" autocomplete="off" id="loanidentifierborr" name="loanidentifierborr" maxlength="255" style="width:120px;border:2px solid black;text-align:center;font-weight:bold;color:black;" value="" />
+										<input type="text" id="loanidentifierborr" autocomplete="off" name="loanidentifierborr" maxlength="255" style="width:120px;border:2px solid black;text-align:center;font-weight:bold;color:black;" value="" />
 									</span>
 								</div>
 								<div style="clear:both;padding-top:6px;float:left;">
@@ -400,7 +400,7 @@ if($isEditor){
 										</select>
 									</span>
 									<span>
-										<a href="../misc/institutioneditor.php?emode=1" target="_blank" title="<?php echo $LANG['ADD_NEW_INST']; ?>">
+										<a href="../misc/institutioneditor.php?emode=1" target="_blank" aria-label="<?php echo $LANG['ADD_NEW_INST']; ?>">
 											<img src="../../images/add.png" style="width:1.2em;" alt="<?php echo $LANG['CREATE_NEW_LOAN']; ?>" />
 										</a>
 									</span>
