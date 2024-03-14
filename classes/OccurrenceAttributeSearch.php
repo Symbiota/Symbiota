53ccd7ffecb0580db676bdc4936226f75017e736
Mark <mark.aaron.fisher@gmail.com>
2024-03-14 14:35:41 -0700

    hide parenthetical from user interface but keep in screen reader

--- classes/OccurrenceAttributeSearch.php
+++ classes/OccurrenceAttributeSearch.php
@@ -78,7 +78,7 @@ class OccurrenceAttributeSearch extends OccurrenceAttributes {
 					$retStr .= '<div title="' . $sArr['description'] . '" style="clear:both;">';
 					$retStr .= '<div style="display:flex;">';
 					$retStr .= '<div><input data-chip="' . $this->traitArr[$traitID]['name'] . ': ' . ucfirst($sArr['name']) . '" name="attr[]" id="traitstateid-' . $sid . '" class="' . $classStr . ' trait-section__input--no-margin" type="checkbox" value="' . $sid . '" onchange="traitChanged(this)" /></div> ';
-					$retStr .= '<div class="trait-section__label--spacer"><label for="traitstateid-' . $sid . '">' . ucfirst($sArr['name']) . ' ('. $this->traitArr[$traitID]['name'] . ')</label></div>';
+					$retStr .= '<div class="trait-section__label--spacer"><label for="traitstateid-' . $sid . '">' . ucfirst($sArr['name']) . '<span class="skip-link"> ('. $this->traitArr[$traitID]['name'] . ')</span></label></div>';
 					$retStr .= '</div>';
 					if($depTraitIdArr){
 						foreach($depTraitIdArr as $depTraitId){
