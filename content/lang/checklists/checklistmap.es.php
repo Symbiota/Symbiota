ef6809d74dc766d7d2279e8209106b58b28bd243
Mark <mark.aaron.fisher@gmail.com>
2024-03-18 19:05:45 -0700

    implement more TVP changes in checklistmap.php

--- content/lang/checklists/checklistmap.es.php
+++ content/lang/checklists/checklistmap.es.php
@@ -11,5 +11,7 @@ $LANG['COORD_MAP'] = 'Mapa de Coordenadas del Listado de Especies';
 $LANG['NO_COORDS'] = 'Su búsqueda aparentemente no contiene ningún registro con coordenadas que puedan ser mapeadas';
 $LANG['MAYBE_RARE'] = 'Puede ser que los ejemplares tengan estado de especie rara/amenazada que requiere que las coordenadas de la localidad permenezcan ocultas';
 $LANG['FAILED_TO_LOAD'] = 'Error al cargar los datos del suceso';
+$LANG['NO_COORDS_HEADER'] = "Sin coordenadas";
+$LANG['MAP_SECTION'] = "Sección de mapa";
 
 ?>
