5745e7bb87490361d35fe16cd4e3260e4eb2d234
Mark <mark.aaron.fisher@gmail.com>
2024-04-08 09:47:31 -0700

    remove fieldset-like styling from duplicate manager

--- collections/datasets/duplicatemanager.php
+++ collections/datasets/duplicatemanager.php
@@ -136,8 +136,7 @@ if($isEditor && $formSubmit){
 		if($isEditor){
 			if(!$action){
 				?>
-				<section class="fieldset-like">
-					<h1> <span> <?php echo (isset($LANG['DUP_LINKAGES']) ? $LANG['DUP_LINKAGES'] : 'Duplicate Linkages'); ?> </span> </h1>
+				<section>
 					<div>
 						<?php echo $LANG['DUP_EXPLANATION']; ?>
 					</div>
