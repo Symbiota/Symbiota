7071db17ec60d52cd1784650be18521816b2b195
Mark <mark.aaron.fisher@gmail.com>
2024-06-05 11:39:00 -0700

    remove extra htmlspecialchars call now that prepared statement is in place

--- classes/OccurrenceManager.php
+++ classes/OccurrenceManager.php
@@ -625,10 +625,10 @@ class OccurrenceManager extends OccurrenceTaxaManager {
 		$retStr = '';
 		foreach($this->searchTermArr as $k => $v){
 			if(is_array($v)) $v = implode(',', $v);
-			if($v) $retStr .= '&'.$this->cleanOutStr($k).'='. htmlspecialchars($this->cleanOutStr($v), ENT_QUOTES);
+			if($v) $retStr .= '&'.$this->cleanOutStr($k).'='. $this->cleanOutStr($v);
 		}
 		if(isset($this->taxaArr['search'])){
-			$retStr .= '&taxa='. htmlspecialchars($this->cleanOutStr($this->taxaArr['search']), ENT_QUOTES);
+			$retStr .= '&taxa='. $this->cleanOutStr($this->taxaArr['search']);
 			if($this->taxaArr['usethes']) $retStr .= '&usethes=1';
 			if(is_numeric($this->taxaArr['taxontype'])) {
 				$retStr .= '&taxontype=' . intval($this->taxaArr['taxontype']);
