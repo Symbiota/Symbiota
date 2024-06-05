b3cf85ab5c8964dc8a488bbd918b4d08fd814c6e
Mark <mark.aaron.fisher@gmail.com>
2024-06-04 17:24:49 -0700

    add better type checking to collIdsFromUrl

--- collections/search/index.php
+++ collections/search/index.php
@@ -569,8 +569,8 @@ $obsArr = (isset($collList['obs'])?$collList['obs']:null);
 		ul.outerWidth(this.element.outerWidth());
 	}
 	const collectionSource = <?php echo isset($collectionSource) ? json_encode($collectionSource) : 'null'; ?>;
-	const collIdsFromUrl = <?php echo isset($collIdsFromUrl) ? json_encode($collIdsFromUrl) : []; ?>;
-	if (collIdsFromUrl.length > 0) {
+	const collIdsFromUrl = <?php echo isset($collIdsFromUrl) ? json_encode($collIdsFromUrl) : 'null'; ?>;
+	if (collIdsFromUrl && Array.isArray(collIdsFromUrl) && collIdsFromUrl.length > 0) {
 		uncheckEverything();
 		checkTheCollectionsThatShouldBeChecked(collIdsFromUrl);
 	}
