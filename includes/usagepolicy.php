2a7c835ba8a662f6e994e08d95e26c433e458693
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-08-23 06:41:53 -0700

    Complete data use policy w/ en & es

--- includes/usagepolicy.php
+++ includes/usagepolicy.php
@@ -8,7 +8,7 @@ $serverHost = UtilityFunctions::getDomain();
 <html lang="en">
 
 <head>
-	<title><?php echo $DEFAULT_TITLE; ?> Data Usage Guidelines</title>
+	<title><?= $DEFAULT_TITLE . ' ' . $LANG['DATA_USAGE_GUIDELINES']?></title>
 	<?php
 
 	include_once($SERVER_ROOT . '/includes/head.php');
@@ -21,11 +21,14 @@ $serverHost = UtilityFunctions::getDomain();
 	include($SERVER_ROOT . '/includes/header.php');
 	?>
 	<div class="navpath">
-		<a href="<?php echo htmlspecialchars($CLIENT_ROOT, ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE); ?>/index.php">Home</a> &gt;&gt;
-		<b>Data Usage Guidelines</b>
+		<a href="<?php echo htmlspecialchars($CLIENT_ROOT, ENT_COMPAT | ENT_HTML401 | ENT_SUBSTITUTE) . '/index.php">' . $LANG['HOME'] . '</a> &gt;&gt' ?>;
+		<b><?= $LANG['DATA_USAGE_GUIDELINES'] ?></b>
 	</div>
 	<!-- This is inner text! -->
 	<div role="main" id="innertext">
+		<?php
+			if($LANG_TAG=='en'){
+		?>
 		<h1 class="page-heading">Guidelines for Acceptable Use of Data</h1>
 		<h2>Recommended Citation Formats</h2>
 		<p>Use one of the following formats to cite data retrieved from the <?php echo $DEFAULT_TITLE; ?> network:</p>
@@ -115,6 +118,101 @@ $serverHost = UtilityFunctions::getDomain();
 		social attitudes and circumstances of the time period when specimens were collected or cataloged.
 		</p>
 	</div>
+	<?php
+		}
+		else{
+	?>
+			<div id="innertext">
+			<h1>Lineamientos para el Uso Aceptable de los Datos</h1><br />
+
+			<h2>Formatos Recomendados de Citas</h2>
+			<div style="margin:10px">
+				Use uno de los siguientes formatos para citar los datos obtenidos del Portal de <?php echo $DEFAULT_TITLE; ?> :
+				<div style="font-weight:bold;margin-top:10px;">
+					Cita General:
+				</div>
+				<div style="margin:10px;">
+					<?php
+					$basePath = "http://";
+					if((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443) $basePath = "https://";
+					$basePath .= $_SERVER["SERVER_NAME"];
+					if($_SERVER["SERVER_PORT"] && $_SERVER["SERVER_PORT"] != 80 && $_SERVER["SERVER_PORT"] != 443) $basePath .= ':'.$_SERVER["SERVER_PORT"];
+					$basePath .= $CLIENT_ROOT.(substr($CLIENT_ROOT,-1)=='/'?'':'/');
+					echo $DEFAULT_TITLE.'. '.date('Y').'. ';
+					echo $basePath.'index.php. ';
+					echo 'Accesado en '.date('F d').'. ';
+					?>
+				</div>
+
+				<div style="font-weight:bold;margin-top:10px;">
+					Uso de datos de ocurrencia de instituciones específicas:
+				</div>
+				<div style="margin:10px;">
+					Biodiversity occurrence data published by: &lt;List of Collections&gt;
+					(Accesado por medio del Portal de <?php echo $DEFAULT_TITLE; ?> ,
+					<?php echo $basePath.'index.php'; ?>, YYYY-MM-DD)<br/><br/>
+					<b>Por ejemplo:</b><br/>
+					Datos de ocurrencia de biodiversidad publicados por:
+					Universidad del Valle de Guatemala, Escuela de Biología USAC, y Centro de Estudios Conservacionistas USAC
+					(Accesado por medio del Portal de <?php echo $DEFAULT_TITLE; ?> ,
+					<?php echo $basePath.'index.php, '.date('Y-m-d').')'; ?>
+				</div>
+			</div>
+			<div>
+			</div>
+
+			<a name="occurrences"></a>
+			<h2>Política de uso de Registros de Ocurrencia</h2>
+		    <div style="margin:10px;">
+				<ul>
+					<li>
+						Mientras que el Portal de <?php echo $DEFAULT_TITLE; ?> hará el mayor esfuerzo posible para controlar y documentar la calidad
+						de los datos que se publican, los datos son compartidos "como están". Cualquier reporte de errores en los datos debería ser
+						dirigido a los curadores o encargados de colecciones apropiados.
+					</li>
+					<li>
+						<?php echo $DEFAULT_TITLE; ?> no puede asumir la responsabilidad por daños resultando por el mal uso o 
+						mala interpretación de los conjuntos de datos, o de errores u omisiones que puedan existir en los datos.
+					</li>
+					<li>
+						Se considera cuestión de ética profesional citar y reconocer el trabajo de otros científicos que
+						ha resultado en el uso de los datos en investigaciones posteriores. Motivamos a los usuarios a 
+						contactar a los investigadores originales responsables por los datos que están accesando.
+					</li>
+					<li>
+						<?php echo $DEFAULT_TITLE; ?> solicita a los usuarios a no redistribuir los datos obtenidos desde este sitio sin el permiso de los propietarios.
+						Sin embargo, enlaces o referencias a este sitio pueden ser publicadas con libertad.
+					</li>
+				</ul>
+		    </div>
+
+			<a name="images"></a>
+			<h2>Images</h2>
+		    <div style="margin:15px;">
+		    	Las imágenes en este sitio web han sido generosamente compartidas por sus autores para
+		    	promover la educación e investigación. Estos contribuyentes retienen los derechos de autor de
+		    	sus imágenes. A menos que se indique lo contrario, las imágenes están disponibles bajo la licencia de Creative Commons
+		    	Attribution-ShareAlike (<a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">CC BY-SA</a>), es decir, compartir con atribución.
+				Los usuarios pueden copiar, transmitir, reusar o adaptar contenido, mientras una atribución
+				indicando la fuente sea realizada. Si el contenido es alterado, transformado o mejorado, 
+				puede ser distribuido únicamente bajo licencias iguales o similares a las originales.
+		    </div>
+
+			<h2>Notas acerca de Registros de Especímenes e Imágenes</h2>
+		    <div style="margin:15px;">
+				Los especímenes son utilizados para investigaciones científicas gracias a que las forma de preparación especializada
+				permite su uso por cientos de años. Algunas colecciones poseen especímenes
+				que fueron colectados hace más de 100 años y que ya no ocurren en el área registrada.
+				Al hacer estos especímenes disponibles en la web como imágenes, su disponibilidad y
+				valor aumenta sin incrementar los posibles daños causados por su uso. Notar que si 
+				considera obtener especímenes, normalmente se requieren permisos de colecta u otros 
+				permisos si se trata de especies amenazadas. Siempre es mejor coordinar los esfuerzos de
+				colecta con instituciones científicas locales que cuentan con colecciones accesibles al público.
+			</div>
+		</div>
+	<?php
+		}
+	?>
 	<?php
 	include($SERVER_ROOT . '/includes/footer.php');
 	?>
