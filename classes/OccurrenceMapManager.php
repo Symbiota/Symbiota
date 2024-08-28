e353a99cd4149454845442188bb5ac17be4e7ce4
Edward Gilbert <egbot@asu.edu>
2024-08-28 14:09:00 -0700

    Modifications
    - "ts.taxauthid = 1" needs to be added to the where condition or the recordset would return duplicate records for each taxonomic thesaurus defined within taxstatus table (e.g. there are 4 defined within SEINet). I suppose not including it would not be a big problem since the occid would be the same for each record, and thus the output data array would just copy over existing data. However, it would iterate through more records than needed. Another solution would be to change the statement to "SELECT DISTINCT...", but this can significantly slows down an ordered query.
    - Since "ts.taxauthid = 1" is added to the where clause, the getTableJoins function should always add the taxstatus left join, thus we can leave out that test.

--- classes/OccurrenceMapManager.php
+++ classes/OccurrenceMapManager.php
@@ -55,18 +55,14 @@ class OccurrenceMapManager extends OccurrenceManager {
 		if($this->sqlWhere){
 			$statsManager = new OccurrenceAccessStats();
 			$sql = 'SELECT o.occid, CONCAT_WS(" ",o.recordedby,IFNULL(o.recordnumber,o.eventdate)) AS identifier, '.
-				'o.sciname, if(ts.family IS NULL, o.family, ts.family) as family, o.tidinterpreted, o.DecimalLatitude, o.DecimalLongitude, o.collid, o.catalognumber, '.
+				'o.sciname, IF(ts.family IS NULL, o.family, ts.family) as family, o.tidinterpreted, o.DecimalLatitude, o.DecimalLongitude, o.collid, o.catalognumber, '.
 				'o.othercatalognumbers, c.institutioncode, c.collectioncode, c.CollectionName '.
 				'FROM omoccurrences o INNER JOIN omcollections c ON o.collid = c.collid ';
 
-			$sqlJoin = $this->getTableJoins($this->sqlWhere);
+			$this->sqlWhere .= 'AND ts.taxauthid = 1 ';
 
-			//Ensure that taxastaus is joined
-			if(strpos($sqlJoin, 'LEFT JOIN taxstatus ts') === false) {
-				$sqlJoin .= 'LEFT JOIN taxstatus ts ON o.tidinterpreted = ts.tid ';
-			}
+			$sql .= $this->getTableJoins($this->sqlWhere);
 
-			$sql .= $sqlJoin;
 			$sql .= $this->sqlWhere;
 
 			if(is_numeric($start) && $limit){
