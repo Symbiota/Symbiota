79b4596aa8c95d6d6ebed16f5eac28331219d555
Edward Gilbert <egbot@asu.edu>
2024-10-14 16:26:20 -0700

    Paleo dev
    - Schema modifications to add columns for time ranges and color codes
    - Add CSV containing html color codes for paleo terms

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -1,2 +1,11 @@
 INSERT INTO `schemaversion` (versionnumber) values ("3.2");
 
+ALTER TABLE `omoccurpaleogts`
+  ADD COLUMN `myaStart` FLOAT NULL DEFAULT NULL AFTER `rankname`,
+  ADD COLUMN `myaEnd` FLOAT NULL DEFAULT NULL AFTER `myaStart`,
+  ADD COLUMN `errorRange` FLOAT NULL DEFAULT NULL AFTER `myaEnd`,
+  ADD COLUMN `colorCode` VARCHAR(10) NULL DEFAULT NULL AFTER `errorRange`;
+
+
+
+
