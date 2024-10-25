d4782473e2472371f9e2786a9fd412a113dda225
MuchQuak <loganpwilt@gmail.com>
2024-10-25 12:01:52 -0700

    remove full text from rawstr because not being used in code base as of now

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -4,6 +4,3 @@ DROP TRIGGER specprocessorrawlabelsfulltext_insert
 DROP TRIGGER specprocessorrawlabelsfulltext_update
 DROP TRIGGER specprocessorrawlabelsfulltext_delete
 DROP TABLE specprocessorawlabelsfulltext;
-
--- Is This even needed if we aren't using it currently?
--- ALTER TABLE specprocessorrawlabels ADD FULLTEXT INDEX(rawstr);
