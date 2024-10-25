2c64ba80fd7ef6eabaad2b13cb273c43e1dbdaab
MuchQuak <loganpwilt@gmail.com>
2024-10-25 11:58:22 -0700

    Drop Table and Triggers specprocessorrawlabelsfulltext
    Drops Specprocessorrawlabelsfulltext table and triggers that aren't
    being used for anything

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -1,2 +1,9 @@
 INSERT INTO `schemaversion` (versionnumber) values ("3.2");
 
+DROP TRIGGER specprocessorrawlabelsfulltext_insert
+DROP TRIGGER specprocessorrawlabelsfulltext_update
+DROP TRIGGER specprocessorrawlabelsfulltext_delete
+DROP TABLE specprocessorawlabelsfulltext;
+
+-- Is This even needed if we aren't using it currently?
+-- ALTER TABLE specprocessorrawlabels ADD FULLTEXT INDEX(rawstr);
