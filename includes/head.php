6e5c86999b54e6001d3db8b6093e265884444a8e
Greg Post <gregory.post@asu.edu>
2024-11-06 15:06:43 -0600

    3.1 styling - the prequel

--- includes/head.php
+++ includes/head.php
@@ -9,7 +9,7 @@
 
 <!-- Symbiota styles -->
 <link href="<?= $CSS_BASE_PATH ?>/symbiota/header.css?ver=<?= $CSS_VERSION ?>" type="text/css" rel="stylesheet">
-<link href="<?= $CSS_BASE_PATH ?>/symbiota/footer.css?ver=<?= $CSS_VERSION ?>" type="text/css" rel="stylesheet">
+<!-- <link href="<?= $CSS_BASE_PATH ?>/symbiota/footer.css?ver=<?= $CSS_VERSION ?>" type="text/css" rel="stylesheet"> -->
 <link href="<?= $CSS_BASE_PATH ?>/symbiota/main.css?ver=<?= $CSS_VERSION ?>" type="text/css" rel="stylesheet">
 <script src="<?= $CLIENT_ROOT ?>/js/symb/lang.js" type="text/javascript"></script>
 <!--
