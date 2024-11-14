999982ffddd6d5aa1a8c3f9abf221ca581663b8e
Katie Pearson <katelin.d.pearson24@gmail.com>
2024-11-14 12:14:30 -0800

    Fix typo

--- content/lang/collections/georef/geolocate.es.php
+++ content/lang/collections/georef/geolocate.es.php
@@ -5,7 +5,7 @@ Language: Español (Spanish)
 ------------------
 */
 $LANG['GEO_LOCATE_TOOL'] = 'Herramienta GEOLocate';
-$LANG['IFRAME PERMISSION'] = 'la URL del iframe no tiene permiso para interactuar conmigo';
+$LANG['IFRAME_PERMISSION'] = 'la URL del iframe no tiene permiso para interactuar conmigo';
 $LANG['NO_POINTS_TO_TRANSFER'] = 'No hay puntos de datos para transferir';
 
 ?>
