6bd794e5bd022aabcd4115fc15906760404bcbbd
Edward Gilbert <egbot@asu.edu>
2024-11-14 17:32:34 -0700

    schema adjustment

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -7,16 +7,15 @@ DROP TABLE specprocessorawlabelsfulltext;
 
 
 ALTER TABLE `omoccuridentifiers`
-  CHANGE COLUMN `identifiervalue` `identifierValue` VARCHAR(75) NOT NULL COLLATE 'utf8mb3_general_ci' AFTER `occid`,
-  CHANGE COLUMN `identifiername` `identifierName` VARCHAR(45) NOT NULL DEFAULT '' COMMENT 'barcode, accession number, old catalog number, NPS, etc' COLLATE 'utf8mb3_general_ci' AFTER `identifierValue`,
+  CHANGE COLUMN `identifiervalue` `identifierValue` VARCHAR(75) NOT NULL AFTER `occid`,
+  CHANGE COLUMN `identifiername` `identifierName` VARCHAR(45) NOT NULL DEFAULT '' AFTER `identifierValue`,
   ADD COLUMN `format` VARCHAR(45) NULL DEFAULT NULL AFTER `identifierName`,
   CHANGE COLUMN `modifiedtimestamp` `modifiedTimestamp` DATETIME NULL DEFAULT NULL AFTER `modifiedUid`,
   CHANGE COLUMN `initialtimestamp` `initialTimestamp` TIMESTAMP NOT NULL DEFAULT current_timestamp() AFTER `modifiedTimestamp`,
   DROP INDEX `UQ_omoccuridentifiers`,
-  ADD UNIQUE INDEX `UQ_omoccuridentifiers` (`occid`, `identifierValue`, `identifierName`) USING BTREE,
+  ADD UNIQUE INDEX `UQ_omoccuridentifiers` (`occid`, `identifierValue`, `identifierName`),
   DROP INDEX `IX_omoccuridentifiers_value`,
-  ADD INDEX `IX_omoccuridentifiers_value` (`identifierValue`) USING BTREE;
-
+  ADD INDEX `IX_omoccuridentifiers_value` (`identifierValue`);
 
 
 ALTER TABLE `kmcharacters` 
