76fd180b898edb5bb9a84a2bfeed999d02ae0b57
Edward Gilbert <egbot@asu.edu>
2024-11-15 16:29:32 -0700

    Schema mods
    - Add taxon and determination fields to taxa and upload tables

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -170,3 +170,16 @@ taxonRemarks)
 SELECT occid, IFNULL(identifiedBy, "unknown"), IFNULL(dateIdentified, "s.d."), family, IFNULL(sciname, "undefined"), scientificName, scientificNameAuthorship, tidInterpreted, identificationQualifier, 
 genus, specificEpithet, taxonRank, infraSpecificEpithet, 1 as isCurrent, identificationReferences, identificationRemarks, taxonRemarks
 FROM omoccurrences;
+
+ALTER TABLE `taxa` 
+  ADD COLUMN `sourceTaxonID` VARCHAR(45) NULL AFTER `source`;
+
+ALTER TABLE `uploadspectemp` 
+ADD COLUMN `taxonID` VARCHAR(45) NULL AFTER `tidinterpreted`,
+ADD COLUMN `taxonConceptID` VARCHAR(45) NULL AFTER `taxonID`,
+ADD COLUMN `identifiedByID` VARCHAR(45) NULL AFTER `identifiedBy`,
+ADD COLUMN `identificationVerificationStatus` VARCHAR(45) NULL AFTER `identificationQualifier`;  
+  
+  
+  
+  
