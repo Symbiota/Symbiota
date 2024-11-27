81f0a99220bc8d1d23cb7a0712399aaa93dc3cf6
Mark <mark.aaron.fisher@gmail.com>
2024-11-26 20:29:39 -0800

    convert en.json and es.json to en.js and es.js, respectively. Move them to better directory for them. Call them directly as a js file

--- collections/search/index.php
+++ collections/search/index.php
@@ -10,8 +10,7 @@ if($LANG_TAG == 'en' || !file_exists($SERVER_ROOT . '/content/lang/collections/s
 else include_once($SERVER_ROOT . '/content/lang/collections/search/index.' . $LANG_TAG . '.php');
 header('Content-Type: text/html; charset=' . $CHARSET);
 
-$filename = file_exists($SERVER_ROOT . '/content/lang/'. $LANG_TAG . '.json') ? $SERVER_ROOT . '/content/lang/'. $LANG_TAG . '.json' : $SERVER_ROOT . '/content/lang/en.json';
-$translations = file_get_contents($filename);
+$filename = file_exists($SERVER_ROOT . '/js/symb/'. $LANG_TAG . '.js') ? $CLIENT_ROOT . '/js/symb/'. $LANG_TAG . '.js' : $CLIENT_ROOT . '/js/symb/en.js';
 
 $dbsWithBracketsRemoved = array_key_exists("db",$_GET) ?  str_replace(array('[',']'), '', $_GET["db"]) : '';
 $explodable = $dbsWithBracketsRemoved;
@@ -562,9 +561,7 @@ $obsArr = (isset($collList['obs'])?$collList['obs']:null);
 	include($SERVER_ROOT . '/includes/footer.php');
 	?>
 </body>
-<script>
-	const translations = <?php echo $translations; ?>
-</script>
+<script src="<?php echo $filename ?>" type="text/javascript"></script>
 <script src="js/searchform.js?ver=1" type="text/javascript"></script>
 <script src="<?php echo $CLIENT_ROOT . '/collections/search/js/alerts.js?v=202107'; ?>" type="text/javascript"></script>
 <script src="<?php echo $CLIENT_ROOT . '/js/symb/api.taxonomy.taxasuggest.js'; ?>" type="text/javascript"></script>
