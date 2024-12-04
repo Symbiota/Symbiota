1ae2b0ecc93216bcf292220792bc780eb1ce2649
MuchQuak <loganpwilt@gmail.com>
2024-12-04 12:16:26 -0800

    add delimeters to preview

--- classes/SpecUpload.php
+++ classes/SpecUpload.php
@@ -238,7 +238,7 @@ class SpecUpload{
 			$fieldName = strtolower($schemaRow->Field);
 			if(!in_array($fieldName,$this->skipOccurFieldArr)){
 				if($fieldName === 'othercatalognumbers' && !$searchVariables) {
-					$occFieldArr[] = 'CASE WHEN u.otherCatalogNumbers IS NULL THEN i.identifiers WHEN i.identifiers IS NULL THEN u.otherCatalogNumbers ELSE CONCAT(u.otherCatalogNumbers, ";", i.identifiers) END as otherCatalogNumbers';
+					$occFieldArr[] = 'CASE WHEN u.otherCatalogNumbers IS NULL THEN i.identifiers WHEN i.identifiers IS NULL THEN u.otherCatalogNumbers ELSE CONCAT(u.otherCatalogNumbers, "; ", i.identifiers) END as otherCatalogNumbers';
 				} else {
 					$occFieldArr[] = $fieldName;
 				}
@@ -246,7 +246,7 @@ class SpecUpload{
 		}
 		$schemaRS->free();
 
-		$identifiers_subquery_join = ' LEFT JOIN (SELECT occid as id_occid, GROUP_CONCAT(CONCAT(identifiername,":" , identifiervalue) SEPARATOR ";") as identifiers from omoccuridentifiers as oi group by oi.occid) as i on i.id_occid = u.occid ';
+		$identifiers_subquery_join = ' LEFT JOIN (SELECT occid as id_occid, GROUP_CONCAT(CONCAT(identifiername,": " , identifiervalue) SEPARATOR ";") as identifiers from omoccuridentifiers as oi group by oi.occid) as i on i.id_occid = u.occid ';
 
 		$sql = 'SELECT occid, dbpk, '.implode(',',$occFieldArr).' FROM uploadspectemp u ' . $identifiers_subquery_join . ' WHERE collid IN('.$this->collId.') ';
 
