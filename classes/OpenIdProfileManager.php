b46f238515c43a92f139847b041be3653e7dc9cc
atticus29 <mark.aaron.fisher@gmail.com>
2025-01-21 17:35:18 -0800

    add cookie-based logout

--- classes/OpenIdProfileManager.php
+++ classes/OpenIdProfileManager.php
@@ -157,6 +157,14 @@ class OpenIdProfileManager extends ProfileManager{
 		$_SESSION = [];
 		session_unset();
 
+		if (ini_get("session.use_cookies")) {
+			$params = session_get_cookie_params();
+			setcookie(session_name(), '', time() - 42000,
+				$params["path"], $params["domain"],
+				$params["secure"], $params["httponly"]
+			);
+		}
+
 		session_destroy();
 
 		$sessionFile = session_save_path() . '/sess_' . $localSessionId;
