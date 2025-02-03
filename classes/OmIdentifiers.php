72aded939202b3d220092031f5dfa33d3c1dd9ad
atticus29 <mark.aaron.fisher@gmail.com>
2025-02-03 15:49:45 -0800

    begin to implement OmIdentifiers.php class

---
+++ classes/OmIdentifiers.php
@@ -0,0 +1,177 @@
+<?php
+include_once('Manager.php');
+include_once('utilities/OccurrenceUtil.php');
+include_once('utilities/UuidFactory.php');
+
+class OmIdentifiers extends Manager
+{
+    private $identifierID = null;
+    private $occid = null;
+    private $schemaMap = array();
+    private $parameterArr = array();
+    private $typeStr = '';
+
+    public function __construct($conn)
+    {
+        parent::__construct(null, 'write', $conn);
+        $this->schemaMap = array(
+            'idomoccuridentifiers' => 'i',
+            'occid' => 'i', // @TODO decide
+            'identifierValue' => 's',
+            'identifierName' => 's',
+            'format' => 's',
+            'notes' => 's',
+            'sortBy' => 'i',
+            'recordID' => 's',
+            'modifiedUid' => 'i',
+            'modifiedTimestamp' => 's', // @TODO make datetime
+            'initialTimestamp' => 's', // @TODO make timestamp
+        );
+    }
+
+    public function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    public function insertIdentifier($inputArr)
+    {
+        $status = false;
+        if ($this->occid) {
+            if (!isset($inputArr['createdUid'])) $inputArr['createdUid'] = $GLOBALS['SYMB_UID'];
+            // $sql = 'INSERT INTO omoccuridentifiers(occid, recordID';
+            // @TODO create a uuid?
+            $sql = 'INSERT INTO omoccuridentifiers ';
+            $sqlValues = '';
+            // $sqlValues = '?, ?, ';
+            // $paramArr = array($this->occid);
+            // $paramArr[] = UuidFactory::getUuidV4();
+            // $this->typeStr = 'is';
+            $this->setParameterArr($inputArr);
+            foreach ($this->parameterArr as $fieldName => $value) {
+                $sql .= ', ' . $fieldName;
+                $sqlValues .= '?, ';
+                $paramArr[] = $value;
+            }
+            $sql .= ') VALUES(' . trim($sqlValues, ', ') . ') ';
+            if ($stmt = $this->conn->prepare($sql)) {
+                $stmt->bind_param($this->typeStr, ...$paramArr);
+                try {
+                    if ($stmt->execute()) {
+                        if ($stmt->affected_rows || !$stmt->error) {
+                            $this->identifierID = $stmt->insert_id;
+                            $status = true;
+                        } else $this->errorMessage = 'ERROR inserting omoccuridentifiers record (2): ' . $stmt->error;
+                    } else $this->errorMessage = 'ERROR inserting omoccuridentifiers record (1): ' . $stmt->error;
+                } catch (mysqli_sql_exception $e) {
+                    if ($e->getCode() == '1062' || $e->getCode() == '1406') {
+                        $this->errorMessage = $e->getMessage();
+                    } else {
+                        throw $e;
+                    }
+                }
+                $stmt->close();
+            } else $this->errorMessage = 'ERROR preparing statement for omoccuridentifiers insert: ' . $this->conn->error;
+        }
+        return $status;
+    }
+
+    private function setParameterArr($inputArr)
+    {
+        foreach ($this->schemaMap as $field => $type) {
+            $postField = '';
+            if (isset($inputArr[$field])) $postField = $field;
+            elseif (isset($inputArr[strtolower($field)])) $postField = strtolower($field);
+            if ($postField) {
+                $value = trim($inputArr[$postField]);
+                if ($value) {
+                    $postField = strtolower($postField);
+                    if ($postField == 'modifiedTimestamp') $value = OccurrenceUtil::formatDate($value);
+                    if ($postField == 'modifieduid') $value = OccurrenceUtil::verifyUser($value, $this->conn);
+                    // if ($postField == 'createduid') $value = OccurrenceUtil::verifyUser($value, $this->conn);
+                    // if ($postField == 'identificationuncertain' || $postField == 'iscurrent' || $postField == 'printqueue' || $postField == 'appliedstatus' || $postField == 'securitystatus') {
+                    // 	if (!is_numeric($value)) {
+                    // 		$value = strtolower($value);
+                    // 		if ($value == 'yes' || $value == 'true') $value = 1;
+                    // 		else $value = 0;
+                    // 	}
+                    // }
+                    // if ($postField == 'sortsequence') {
+                    if ($postField == 'sortBy') { // @TODO ? sortBy same a sortsequence?
+                        if (!is_numeric($value)) $value = 10;
+                    }
+                } else $value = null;
+                $this->parameterArr[$field] = $value;
+                $this->typeStr .= $type;
+            }
+        }
+        if (isset($inputArr['occid']) && $inputArr['occid'] && !$this->occid) $this->occid = $inputArr['occid'];
+    }
+
+    public function updateIdentifier($inputArr)
+    {
+        $status = false;
+        if ($this->identifierID && $this->conn) {
+            // $occidAssociate = false;
+            // if (isset($inputArr['identifierName']) && $inputArr['identifierName']) {
+            //     $targetIdentifier = $this->getIdentifierOccid($inputArr['occid']); // @TODO global replace misspelled getOccidAsscoiate
+            // }
+            // elseif (isset($inputArr['object-occurrenceID']) && $inputArr['object-occurrenceID']) {
+            //     $targetIdentifier = $this->getOccidAsscoiate($inputArr['object-occurrenceID'], 'occurrenceID');
+            // }
+            // if ($targetIdentifier) {
+            //     $inputArr['targetIdentifier'] = $targetIdentifier;
+            // } elseif ($occidAssociate !== false) {
+            //     $this->errorMessage = 'Unable to locate internal association record';
+            //     return false;
+            // }
+            $this->setParameterArr($inputArr);
+            $paramArr = array();
+            $sqlFrag = '';
+            foreach ($this->parameterArr as $fieldName => $value) {
+                $sqlFrag .= $fieldName . ' = ?, ';
+                $paramArr[] = $value;
+            }
+            $paramArr[] = $this->identifierID; // @TODO why?
+            $this->typeStr .= 'i';
+            $sql = 'UPDATE IGNORE omoccuridentifiers SET ' . trim($sqlFrag, ', ') . ' WHERE (occid = ? AND identifierName = ?)';
+            if ($stmt = $this->conn->prepare($sql)) {
+                $stmt->bind_param($this->typeStr, ...$paramArr);
+                $stmt->execute();
+                if ($stmt->affected_rows || !$stmt->error) $status = true;
+                else $this->errorMessage = 'ERROR updating omoccurassociations record: ' . $stmt->error;
+                $stmt->close();
+            } else $this->errorMessage = 'ERROR preparing statement for updating omoccurassociations: ' . $this->conn->error;
+        }
+        return $status;
+    }
+
+    public function getIdentifier($occid, $identifierName)
+    {
+        $idomoccuridentifiers = null;
+        // $identifier = trim($identifier);
+        // if ($identifier) {
+        $sql = 'SELECT idomoccuridentifiers FROM omoccuridentifiers WHERE occid = ? OR identifierName = ?';
+        // if ($target == 'catalogNumber') $sql = 'SELECT occid FROM omoccurrences WHERE catalogNumber = ?';
+        if ($stmt = $this->conn->prepare($sql)) {
+            // if ($target == 'catalogNumber') $stmt->bind_param('s', $identifier);
+            $stmt->bind_param('is', $occid, $identifierName);
+            $stmt->execute();
+            $stmt->bind_result($idomoccuridentifiers);
+            $stmt->fetch();
+            $stmt->close();
+        }
+        // }
+        return $idomoccuridentifiers;
+    }
+
+    public function setOccid($id)
+    {
+        if (is_numeric($id)) $this->occid = $id;
+    }
+
+    public function getSchemaMap()
+    {
+        return $this->schemaMap;
+    }
+}
