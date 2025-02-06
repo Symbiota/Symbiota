076d98d1cd915ebbe4cf0b05a32b542fc52b3566
NikitaSalikov <86389284+NikitaSalikov@users.noreply.github.com>
2025-02-06 14:40:03 -0700

    moved the paleo table join to getTableJoins (#2189)

--- classes/OccurrenceListManager.php
+++ classes/OccurrenceListManager.php
@@ -38,8 +38,6 @@ class OccurrenceListManager extends OccurrenceManager{
 		if ($GLOBALS['ACTIVATE_PALEO'])
 			$sql .= ', paleo.formation, paleo.earlyInterval, paleo.lateInterval ';
 		$sql .= 'FROM omoccurrences o INNER JOIN omcollections c ON o.collid = c.collid ';
-		if ($GLOBALS['ACTIVATE_PALEO'])
-			$sql .= 'LEFT JOIN omoccurpaleo paleo ON o.occid = paleo.occid ';
 		$sql .= $this->getTableJoins($sqlWhere).$sqlWhere;
 		//Don't allow someone to query all occurrences if there are no conditions
 		if(!$sqlWhere) $sql .= 'WHERE o.occid IS NULL ';
