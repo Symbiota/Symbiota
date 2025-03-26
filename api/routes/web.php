c8880c9d017fa10d5b1710d758ed54efc74e7d44
Mark <mark.aaron.fisher@gmail.com>
2025-03-26 11:02:57 -0700

    rearrange the routes to avoid route parsing conflicts (#2328)

--- api/routes/web.php
+++ api/routes/web.php
@@ -32,12 +32,12 @@ $router->group(['prefix' => 'v2'], function () use ($router) {
 	$router->get('collection/{id}', ['uses' => 'CollectionController@showOneCollection']);
 
 	$router->get('occurrence',  ['uses' => 'OccurrenceController@showAllOccurrences']);
+	$router->get('occurrence/annotation', ['uses' => 'OccurrenceAnnotationController@showAllAnnotations']);
 	$router->get('occurrence/{id}', ['uses' => 'OccurrenceController@showOneOccurrence']);
 	$router->get('occurrence/{id}/media', ['uses' => 'OccurrenceController@showOneOccurrenceMedia']);
 	$router->get('occurrence/{id}/identification', ['uses' => 'OccurrenceController@showOneOccurrenceIdentifications']);
 	$router->get('occurrence/{id}/annotation', ['uses' => 'OccurrenceAnnotationController@showOccurrenceAnnotations']);
 	$router->get('occurrence/{id}/reharvest', ['uses' => 'OccurrenceController@oneOccurrenceReharvest']);
-	$router->get('occurrence/annotation', ['uses' => 'OccurrenceAnnotationController@showAllAnnotations']);
 	$router->post('occurrence/skeletal', ['uses' => 'OccurrenceController@skeletalImport']);
 
 	$router->get('installation',  ['uses' => 'InstallationController@showAllPortals']);
