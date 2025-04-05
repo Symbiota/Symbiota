0bd69706175af9a732b87dbfe4153e92daebf5da
Edward Gilbert <egbot@asu.edu>
2025-04-04 17:46:13 -0700

    Add API version

--- config/schema/3.0/patches/db_schema_patch-3.3-portalindex.sql
+++ config/schema/3.0/patches/db_schema_patch-3.3-portalindex.sql
@@ -2,5 +2,7 @@
 
 ALTER TABLE `portalindex` 
   ADD COLUMN `lastContact` TIMESTAMP NULL AFTER `notes`,
-  ADD COLUMN `modifiedTimestamp` TIMESTAMP NULL AFTER `lastContact`;
+  ADD COLUMN `modifiedTimestamp` TIMESTAMP NULL AFTER `lastContact`,
+  ADD COLUMN `apiVersion` VARCHAR(45) NULL AFTER `symbiotaVersion`;
+
 
