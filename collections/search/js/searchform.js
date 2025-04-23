8742dcb085820e60265e2d95cc1cfce9fee8df25
atticus29 <mark.aaron.fisher@gmail.com>
2025-04-22 17:53:25 -0700

    address code review feedback from Nikita

--- collections/search/js/searchform.js
+++ collections/search/js/searchform.js
@@ -578,7 +578,7 @@ function getParam(paramName) {
  * Creates search URL with parameters
  * Define parameters to be looked for in `paramNames` array
  */
-function getSearchUrl() {
+function getSearchUrl(appendParams = false) {
   const formatPreference = document.getElementById("list-button").checked
     ? "list"
     : "table";
@@ -588,20 +588,23 @@ function getSearchUrl() {
 
   const baseUrl = new URL(harvestUrl + urlSuffix);
 
-  // Clears array temporarily to avoid redundancy
-  paramsArr = {};
+  if(appendParams){
+    // Clears array temporarily to avoid redundancy
+    paramsArr = {};
+  
+    // Grabs params from form for each param name
+    paramNames.forEach((param, i) => {
+      return getParam(paramNames[i]);
+    });
+  
+    // Appends each key value for each param in search url
+    let queryString = Object.keys(paramsArr).map((key) => {
+      baseUrl.searchParams.append(key, paramsArr[key]);
+    });
+  
+    baseUrl.searchParams.append("comingFrom", "newsearch");
+  }
 
-  // Grabs params from form for each param name
-  paramNames.forEach((param, i) => {
-    return getParam(paramNames[i]);
-  });
-
-  // Appends each key value for each param in search url
-  let queryString = Object.keys(paramsArr).map((key) => {
-		baseUrl.searchParams.append(key, paramsArr[key]);
-  });
-
-  baseUrl.searchParams.append("comingFrom", "newsearch");
   return baseUrl.href;
 }
 
@@ -728,6 +731,7 @@ function simpleSearch() {
     sessionStorage.setItem('verbatimSearchUrl', searchUrl);
     const submitForm = document.getElementById("params-form");
     submitForm.method = "POST"; // if GET is used instead, the URL is too short for complex polygon + many collections queries. Hence, the need for POST.
+    submitForm.action = getSearchUrl();
     submitForm.submit();
   } else {
     handleValErrors(errors);
@@ -735,7 +739,7 @@ function simpleSearch() {
 }
 
 function shortenSearchUrlIfAllCollectionsAreSearched(){
-  const searchUrlOriginal = getSearchUrl();
+  const searchUrlOriginal = getSearchUrl(true);
     let searchUrl = searchUrlOriginal;
     if(searchUrlOriginal.includes("db=all")){
       searchUrl = searchUrlOriginal.replace(/db=all(?:%[^&?]*)*/, "db=all");
