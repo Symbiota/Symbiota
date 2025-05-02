57e97c676536012fe9a29c0faaafee447b1f9fa2
atticus29 <mark.aaron.fisher@gmail.com>
2025-05-01 21:29:01 -0700

    remove files accidentally tracked in this PR

--- copyUntracked.sh
+++
@@ -1,6 +0,0 @@
-cp includes/header_template.php includes/header.php
-cp includes/footer_template.php includes/footer.php
-cp includes/head_template.php includes/head.php
-cp includes/usagepolicy_template.php includes/usagepolicy.php
-cp includes/minimalheader_template.php includes/minimalheader.php
-cp index_template.php index.php
