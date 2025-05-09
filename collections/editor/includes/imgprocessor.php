9efa340aea84e0b1eda781decb764ceec400882d
James Mickley <james.mickley@oregonstate.edu>
2025-05-09 09:23:00 -0700

    Minor CSS formatting changes

--- collections/editor/includes/imgprocessor.php
+++ collections/editor/includes/imgprocessor.php
@@ -53,7 +53,7 @@ else include_once($SERVER_ROOT.'/content/lang/collections/editor/includes/imgpro
 	}
 </script>
 <style>
-	.ocr-box{ padding: 5px; float:left; }
+	.ocr-box{ padding: 10px; float:left; }
 	.ocr-box button{ margin: 5px; }
 </style>
 <div id="labelProcDiv" style="width:100%;height:1050px;position:relative">
@@ -120,17 +120,17 @@ else include_once($SERVER_ROOT.'/content/lang/collections/editor/includes/imgpro
 							<legend>VoucherVision OCR</legend>
 
 							<input type="checkbox" id="ocrOnly" value="1" /> <?php echo $LANG['VV_OCR_ONLY']; ?><br/>
-							<div style="font-weight: bold; position: relative; left: -6px; margin-bottom: 4px;"> <?php echo $LANG['VV_OCR_ENGINES']; ?></div>
+							<div style="font-weight: bold; position: relative; left: -6px; margin: 5px 0px;"> <?php echo $LANG['VV_OCR_ENGINES']; ?></div>
 							<input type="checkbox" name="engines" id="gemini-1.5-pro" value="1" /> Gemini 1.5 Pro<br/>
 							<input type="checkbox" name="engines" id="gemini-2.0-flash" value="1" checked/> Gemini 2.0 Flash<br/>
-							<div style="font-weight: bold; position: relative; left: -6px; margin-bottom: 4px;"> <?php echo $LANG['VV_TRANSCRIPTION_MODEL']; ?></div>
+							<div style="font-weight: bold; position: relative; left: -6px; margin: 5px 0px;"> <?php echo $LANG['VV_TRANSCRIPTION_MODEL']; ?></div>
 							<select id="llm-model">
 								<option value="gemini-2.0-flash">Gemini 2.0 Flash</option>
 								<option value="gemini-2.0-pro">Gemini 1.5 Pro</option>
 								<option value="gemini-2.5-pro">Gemini 2.5 Flash</option>
 								<option value="gemini-2.5-pro">Gemini 2.5 Pro</option>
 							</select><br/>
-							<div style="font-weight: bold; position: relative; left: -6px; margin-bottom: 4px;"> <?php echo $LANG['VV_TRANSCRIPTION_PROMPT']; ?></div>
+							<div style="font-weight: bold; position: relative; left: -6px; margin: 5px 0px;"> <?php echo $LANG['VV_TRANSCRIPTION_PROMPT']; ?></div>
 							<select id="prompt">
 								<option value="OSC_Symbiota">OSC_Symbiota</option>
 								<option value="SLTPvM_default">SLTPvM_default</option>
