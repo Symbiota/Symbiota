4a99a8c0b43a18832ac92759edf2b0224f130d56
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-06-26 15:47:56 -0700

    Change wording of geographic validator tool

--- content/lang/collections/cleaning/coordinatevalidator.en.php
+++ content/lang/collections/cleaning/coordinatevalidator.en.php
@@ -5,21 +5,23 @@ Language: English
 ------------------
 */
 $LANG['COOR_VALIDATOR'] = 'Coordinate Validator';
-$LANG['TOOL_DESCRIPTION'] = 'Click "Validate All Coordinates" button to loop through all unvalidated georeferenced specimens and verify that the coordinates actually fall within the defined political units.Click on the list symbol to display specimens of that ranking. If there was a mismatch between coordinates and county, this could be due to:';
-$LANG['COORDINATES_OUTSIDE_COUNTY_LIMITS'] = 'Coordinates fall outside of county limits';
-$LANG['WRONG_COUNTY_ENTERED'] = 'Coordinates fall outside of county limits';
-$LANG['COUNTY_MISSPELLED'] = 'County is misspelled';
-$LANG['VALIDATION_COUNT_LIMIT'] = '* Note coordinate validation is limited to 50000 records at a time, but can be ran mutliple times';
-$LANG['POPULATE_COUNTRY'] = 'Populate country if missing and validated';
-$LANG['POPULATE_STATE_PROVINCE'] = 'Populate State/Province if missing and validated';
-$LANG['POPULATE_COUNTY'] = 'Populate county if missing and validated';
+$LANG['TOOL_DESCRIPTION'] = 'Clicking the "Validate All Coordinates" button will loop through all unvalidated georeferenced specimens to verify that 
+    the coordinates actually fall within the defined geographic units, as defined by geographic polygons stored in the geographic thesaurus. 
+    Click on the list symbol to display specimens of that ranking. If there was a mismatch between coordinates and county, this could be due to:';
+$LANG['COORDINATES_OUTSIDE_COUNTY_LIMITS'] = "Coordinates fall outside of geographic unit's limits";
+$LANG['WRONG_COUNTY_ENTERED'] = 'Wrong geographic unit was entered';
+$LANG['COUNTY_MISSPELLED'] = 'Geographic unit is misspelled';
+$LANG['VALIDATION_COUNT_LIMIT'] = '* Note coordinate validation is limited to 50000 records at a time, but can be run mutliple times.';
+$LANG['POPULATE_COUNTRY'] = 'Populate country if missing and can be inferred from coordinates';
+$LANG['POPULATE_STATE_PROVINCE'] = 'Populate state/province if missing and can be inferred from coordinates';
+$LANG['POPULATE_COUNTY'] = 'Populate county if missing and can be inferred from coordinates';
 $LANG['VALIDATE_ALL_COORDINATES'] = 'Validate All Coordinates';
 $LANG['RANKING_STATISTICS'] = 'Ranking Statistics';
 
 $LANG['RANKING'] = 'Ranking';
 $LANG['STATUS'] = 'Status';
 $LANG['COUNT'] = 'Count';
-$LANG['UNVERIFIED'] = 'Unverified';
+$LANG['UNVERIFIED'] = 'unverified';
 $LANG['ALL_COORDINATES_VALIDATED'] = 'All coordinates been validated';
 
 ?>
