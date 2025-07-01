be58b2abcd6ca694e3abbad2e514b36485a9e8fa
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-07-01 08:48:28 -0700

    Add language tags, change breadcrumbs, add link title

--- content/lang/collections/cleaning/coordinatevalidator.en.php
+++ content/lang/collections/cleaning/coordinatevalidator.en.php
@@ -4,6 +4,9 @@
 Language: English
 ------------------
 */
+$LANG['HOME'] = 'Home';
+$LANG['COLLECTION_MANAGEMENT'] = 'Collection Management';
+
 $LANG['COOR_VALIDATOR'] = 'Coordinate Validator';
 $LANG['TOOL_DESCRIPTION'] = 'Clicking the "Validate All Coordinates" button will loop through all unvalidated georeferenced specimens to verify that 
     the coordinates actually fall within the defined geographic units, as defined by geographic polygons stored in the geographic thesaurus. 
@@ -11,16 +14,29 @@ $LANG['TOOL_DESCRIPTION'] = 'Clicking the "Validate All Coordinates" button will
 $LANG['COORDINATES_OUTSIDE_COUNTY_LIMITS'] = "Coordinates fall outside of geographic unit's limits";
 $LANG['WRONG_COUNTY_ENTERED'] = 'Wrong geographic unit was entered';
 $LANG['COUNTY_MISSPELLED'] = 'Geographic unit is misspelled';
-$LANG['VALIDATION_COUNT_LIMIT'] = '* Note coordinate validation is limited to 50000 records at a time, but can be run mutliple times.';
+$LANG['VALIDATION_COUNT_LIMIT'] = '* Note coordinate validation is limited to 50000 records at a time, but can be run multiple times.';
+$LANG['LAST_VER_DATE'] = 'Last Verification Date';
+$LANG['RECORDS_TOOK'] = 'records took';
+$LANG['SEC'] = 'seconds';
+$LANG['SPEC_RANK_OF'] = 'Specimen with rank of';
+$LANG['CHECKED_BY'] = 'checked by';
+ $LANG['NOTHING_TO_DISPLAY'] = 'Nothing to be displayed';
 $LANG['POPULATE_COUNTRY'] = 'Populate country if missing and can be inferred from coordinates';
 $LANG['POPULATE_STATE_PROVINCE'] = 'Populate state/province if missing and can be inferred from coordinates';
 $LANG['POPULATE_COUNTY'] = 'Populate county if missing and can be inferred from coordinates';
+$LANG['RE-VALIDATE_ALL_COORDINATES'] = 'Re Validate All Coordinates';
 $LANG['VALIDATE_ALL_COORDINATES'] = 'Validate All Coordinates';
 $LANG['RANKING_STATISTICS'] = 'Ranking Statistics';
 
 $LANG['RANKING'] = 'Ranking';
 $LANG['STATUS'] = 'Status';
 $LANG['COUNT'] = 'Count';
+$LANG['RE-VERIFY'] = 'Re-Verify';
 $LANG['UNVERIFIED'] = 'unverified';
+
+$LANG['NON-VERIFIED_BY_COUNTRY'] = 'Non-verified listed by country';
+$LANG['COUNTRY'] = 'Country';
+$LANG['VIEW_SPECIMENS'] = 'View Specimens';
+$LANG['NOT_AUTHORIZED'] = 'You are not authorized to access this page';
 
 ?>
