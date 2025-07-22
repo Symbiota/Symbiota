0258c57f228d7ec5564b9dc12a9eb9e2603e5588
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2025-07-22 15:07:45 -0700

    Update db_schema_patch-3.2.sql

--- config/schema/3.0/patches/db_schema_patch-3.2.sql
+++ config/schema/3.0/patches/db_schema_patch-3.2.sql
@@ -383,6 +383,7 @@ CREATE FUNCTION `swap_wkt_coords`(str TEXT) RETURNS text
     DECLARE cha BINARY;
     DECLARE flipped TEXT;
     DECLARE firstPt TEXT;
+    DECLARE flippedPoint TEXT;
 
     SET i = 0;
     SET flipped = '';
@@ -395,23 +396,22 @@ CREATE FUNCTION `swap_wkt_coords`(str TEXT) RETURNS text
         IF cha REGEXP '^[A-Za-z(),]' THEN
           IF latStart is not null and latEnd is not null and lngStart is not null THEN
             SET lngEnd = i;
-            SET flipped = CONCAT(flipped, 
+            SET flippedPoint = CONCAT(
               SUBSTRING(str, lngStart, CASE WHEN lngStart = lngEnd THEN 1 ELSE lngEnd - lngStart END),
               " ", 
               SUBSTRING(str, latStart, CASE WHEN latStart = latEnd THEN 1 ELSE latEnd - latStart END)
-            );
+			      );
+            SET flipped = CONCAT(flipped, flippedPoint);
 
             IF firstPt is null THEN
-              SET firstPt = CONCAT(
-                SUBSTRING(str, lngStart, CASE WHEN lngStart = lngEnd THEN 1 ELSE lngEnd - lngStart END),
-                " ",
-                SUBSTRING(str, latStart, CASE WHEN latStart = latEnd THEN 1 ELSE latEnd - latStart END)
-              );
+              SET firstPt = flippedPoint;
             END IF;
 
             IF cha = ')' AND firstPt is not null and flipped not like CONCAT('%', firstPt) THEN
-              -- ST_GEOMFROMTEXT requires a polygon's first and last points be the same, so add the first point at the end if needed.
-              SET flipped = CONCAT(flipped, ',', firstPt);
+              IF flipped not like CONCAT('%', firstPt) THEN
+                -- ST_GEOMFROMTEXT requires a polygon's first and last points be the same, so add the first point at the end if needed.
+                SET flipped = CONCAT(flipped, ',', firstPt);
+              END IF;
               SET firstPt = NULL;
             END IF;
 
