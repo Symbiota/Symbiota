bffcfcb833954b687bb8e47f382c284813231ba8
MuchQuak <loganpwilt@gmail.com>
2025-08-18 13:50:57 -0700

    Rebuilding to have segmented search

--- collections/editor/batchDuplicateMerge.php
+++ collections/editor/batchDuplicateMerge.php
@@ -19,8 +19,7 @@ $fields = [
 	'occid',
 	'collid',
 	'catalognumber',
-	'duplicateid',
-	'collid',
+	// 'duplicateid',
 	'country',
 	'stateProvince',
 	'county',
@@ -45,7 +44,6 @@ $shownFields = [
 	'catalognumber',
 	'institutionCode',
 	'collectionCode',
-	'duplicateid',
 	'country',
 	'stateProvince',
 	'county',
@@ -79,85 +77,16 @@ function mapField($field, $prefix) {
 	return $tableAlias . $prefix . '.' . $field . ($prefix? ' AS ' . $field . $prefix: '');
 };
 
-// Don't show in ui table
-$fieldIgnores = [
-	//'collid',
-	'duplicateid',
-];
+function getTargets(int $collId) {
+	global $fields, $harvestFields;
 
-foreach($_POST as $targetOccId => $sourceOccId) {
-	if(is_numeric($targetOccId) && is_numeric($sourceOccId)) {
-		try {
-			copyOccurrenceInfo($targetOccId, $sourceOccId, $harvestFields);
-			array_push($updated, $targetOccId);
-		} catch(Exception $e)  {
-			$errors[$targetOccId] = $e->getMessage();
-		}
-	}
-}
+	$sql = 'SELECT DISTINCT duplicateid from omoccurduplicatelink dl
+	join omoccurrences o on o.occid = dl.occid and o.collid = ?
+	ORDER BY duplicateid LIMIT 100';
 
-$sql = 'SELECT DISTINCT ' . 
-	getSqlFields($fields) . ',' . 
-	getSqlFields($harvestFields) . ',' . 
-	getSqlFields($fields, '2') . ',' . 
-	getSqlFields($harvestFields, '2') . ' from omoccurduplicatelink dl2
-	join omoccurrences o2 on o2.occid = dl2.occid and o2.collid = ?
-	join omoccurduplicatelink dl on dl.duplicateid = dl2.duplicateid
-	join omoccurrences o on o.occid = dl.occid where o.occid != o2.occid';
-
-$parameters = [$collid];
-
-$customWhere = CustomQuery::buildCustomWhere($_REQUEST, 'o');
-if($customWhere['sql']) {
-	$sql .= ' and (' . $customWhere['sql'] . ')';
-
-	$parameters = array_merge($parameters, $customWhere['bindings']);
-}
-
-$sql .= ' ORDER BY duplicateid LIMIT 100';
-
-$conn = Database::connect('readonly');
-$rs = QueryUtil::executeQuery($conn, $sql, $parameters);
-$duplicates = $rs->fetch_all(MYSQLI_ASSOC);
-
-$rs = QueryUtil::executeQuery($conn, 'SELECT collid, collectionCode, institutionCode from omcollections', []);
-$collections = [];
-foreach($rs->fetch_all(MYSQLI_ASSOC) as $row) {
-	$collections[$row['collid']] = $row;
-}
-
-$targets  = [];
-$options = [];
-$tableHeaders = '';
-
-function getShownDuplicate(array $occurrenceDuplicate, string $postfix = '') {
-	global $shownFields;
-
-	$shownDuplicate = [];
-	foreach($shownFields as $field) {
-		$shownDuplicate[$field] = $occurrenceDuplicate[$field . $postfix] ?? null;
-	}
-
-	return $shownDuplicate;
-}
-
-foreach ($duplicates as $dupe) {
-	if(!isset($options[$dupe['duplicateid']])) {
-		$options[$dupe['duplicateid']] = [];
-	}
-
-	if($dupe['collid2'] === $collid) {
-		$targets[$dupe['occid2']] = getShownDuplicate($dupe, '2');
-		$targets[$dupe['occid2']]['institutionCode'] = $collections[$dupe['collid2']]['institutionCode'];
-		$targets[$dupe['occid2']]['collectionCode'] = $collections[$dupe['collid2']]['collectionCode'];
-		$targets[$dupe['occid2']]['duplicateid'] = $dupe['duplicateid2'];
-	}
-
-	$shownDupe = getShownDuplicate($dupe);
-	$shownDupe['duplicateid'] = $dupe['duplicateid'];
-	$shownDupe['institutionCode'] = $collections[$dupe['collid']]['institutionCode'];
-	$shownDupe['collectionCode'] = $collections[$dupe['collid']]['collectionCode'];
-	$options[$dupe['duplicateid']][] = $shownDupe;
+	$conn = Database::connect('readonly');
+	$rs = QueryUtil::executeQuery($conn, $sql, [$collId]);
+	return array_map(fn($v) => $v['duplicateid'], $rs->fetch_all(MYSQLI_ASSOC));
 }
 
 function getTableHeaders(array $arr, array $ignore = []) {
@@ -177,7 +106,7 @@ function getTableHeaders(array $arr, array $ignore = []) {
 	return $html;
 }
 
-function render_row($row, $checkboxName = false, $fieldIgnores = []) {
+function render_row($row, $checkboxName = false, $shownFields = []) {
 	$html = '<tr>';
 	$html .= '<td><div style="display:flex; align-items:center; justify-content: center;">' . 
 		($checkboxName ? '<input type="checkbox" onclick="checkbox_one_only(this)" name="'. $checkboxName  .'" value="' . $row['occid'] . '" style="margin:0"/>': '') . 
@@ -186,15 +115,25 @@ function render_row($row, $checkboxName = false, $fieldIgnores = []) {
 	$base_url = ($GLOBALS['CLIENT_ROOT']? $GLOBALS['CLIENT_ROOT'] . '/': '') . 
 	'/collections/individual/index.php?occid=';
 		
-	foreach ($row as $key => $value) {
-		if(in_array($key, $fieldIgnores)) {
-			continue;
-		} else if($key === 'occid') {
+
+	foreach($shownFields as $key) {
+		$value = $row[$key] ?? null;
+		if($key === 'occid') {
 			$html .= '<td><a href="'. $base_url . $value . '">' . $value . '</a></td>';
 		}  else  {
 			$html .= '<td>' . $value . '</td>';
 		}
 	}
+
+	// foreach ($row as $key => $value) {
+	// 	if(in_array($key, $fieldIgnores)) {
+	// 		continue;
+	// 	} else if($key === 'occid') {
+	// 		$html .= '<td><a href="'. $base_url . $value . '">' . $value . '</a></td>';
+	// 	}  else  {
+	// 		$html .= '<td>' . $value . '</td>';
+	// 	}
+	// }
 
 	return $html .=  '</tr>';
 }
@@ -228,6 +167,145 @@ function isCopyable($target, $source) {
 		}
 	}
 }
+
+function getShownDuplicate(array $occurrenceDuplicate, string $postfix = '') {
+	global $shownFields;
+
+	$shownDuplicate = [];
+	foreach($shownFields as $field) {
+		$shownDuplicate[$field] = $occurrenceDuplicate[$field . $postfix] ?? null;
+	}
+
+	return $shownDuplicate;
+}
+
+function getOccurrences(array $occIds, mysqli $conn) {
+	global $fields, $harvestFields; 
+
+	$parameters = str_repeat('?,', count($occIds) - 1) . '?';
+
+	$sql = 'SELECT ' . getSqlFields($fields) . ',' .getSqlFields($harvestFields) . 
+	' from omoccurrences o where occid in (' . $parameters . ')';
+
+	$rs = QueryUtil::executeQuery($conn, $sql, $occIds);
+
+	return $rs->fetch_all(MYSQLI_ASSOC);
+}
+
+function searchDuplicateOptions(int $targetCollId, mysqli $conn) {
+	$sql = 'SELECT dl.duplicateid, o2.occid as targetOccid, o.occid from omoccurduplicatelink dl2
+	join omoccurrences o2 on o2.occid = dl2.occid and o2.collid = ?
+	join omoccurduplicatelink dl on dl.duplicateid = dl2.duplicateid
+	join omoccurrences o on o.occid = dl.occid where o.occid != o2.occid';
+
+	$parameters = [$targetCollId];
+
+	$customWhere = CustomQuery::buildCustomWhere($_REQUEST, 'o');
+	if($customWhere['sql']) {
+		$sql .= ' and (' . $customWhere['sql'] . ')';
+
+		$parameters = array_merge($parameters, $customWhere['bindings']);
+	}
+
+	$sql .= ' LIMIT 100';
+
+	$rs = QueryUtil::executeQuery($conn, $sql, $parameters);
+
+	return $rs->fetch_all(MYSQLI_ASSOC);
+}
+
+function getCollections(mysqli $conn) {
+	$rs = QueryUtil::executeQuery($conn, 'SELECT collid, collectionCode, institutionCode from omcollections', []);
+	$collections = [];
+	foreach($rs->fetch_all(MYSQLI_ASSOC) as $row) {
+		$collections[$row['collid']] = $row;
+	}	
+
+	return $collections;
+}
+
+function copyInfo() {
+	global $errors, $harvestFields;
+	foreach($_POST as $targetOccId => $sourceOccId) {
+		if(is_numeric($targetOccId) && is_numeric($sourceOccId)) {
+			try {
+				copyOccurrenceInfo($targetOccId, $sourceOccId, $harvestFields);
+				array_push($updated, $targetOccId);
+			} catch(Exception $e)  {
+				$errors[$targetOccId] = $e->getMessage();
+			}
+		}
+	}
+}
+
+$conn = Database::connect('readonly');
+
+$duplicates = searchDuplicateOptions($collid, $conn);
+$collections = getCollections($conn);
+
+$targets  = [];
+$options = [];
+
+$targetsOccids = [];
+$optionOccids = [];
+
+foreach ($duplicates as $dupe) {
+	$occid = $dupe['occid'];
+	$targetOccid= $dupe['targetOccid'];
+	$duplicateId = $dupe['duplicateid'];
+
+	if(!isset($targets[$targetOccid])) {
+		$targets[$targetOccid] = $duplicateId;
+		$targetsOccids[] = $targetOccid;
+	}
+
+	if(!isset($options[$duplicateId])) {
+		$options[$duplicateId] = [];
+	}
+
+	if(!isset($options[$duplicateId][$occid])) {
+		$options[$duplicateId][$occid] = [];
+		$optionOccids[$occid] = $duplicateId;
+	}
+}
+
+foreach (getOccurrences($targetsOccids, $conn) as $target) {
+	$target['duplicateid'] = $targets[$target['occid']]; 
+	$target['institutionCode'] = $collections[$target['collid']]['institutionCode'];
+	$target['collectionCode'] = $collections[$target['collid']]['collectionCode'];
+	$targets[$target['occid']] =  $target;
+}
+
+foreach (getOccurrences(array_keys($optionOccids), $conn) as $option) {
+	$occid = $option['occid'];
+	$duplicateId = $optionOccids[$occid];
+	$option['duplicateid'] = $duplicateId;
+	$option['institutionCode'] = $collections[$option['collid']]['institutionCode'];
+	$option['collectionCode'] = $collections[$option['collid']]['collectionCode'];
+	$options[$duplicateId][$occid] = $option;
+}
+
+//var_dump($targetsOccurrneces, $optionsOccurrences);
+
+
+// foreach ($duplicates as $dupe) {
+// 	if(!isset($options[$dupe['duplicateid']])) {
+// 		$options[$dupe['duplicateid']] = [];
+// 	}
+//
+// 	if($dupe['collid2'] === $collid) {
+// 		$targets[$dupe['occid2']] = getShownDuplicate($dupe, '2');
+// 		$targets[$dupe['occid2']]['institutionCode'] = $collections[$dupe['collid2']]['institutionCode'];
+// 		$targets[$dupe['occid2']]['collectionCode'] = $collections[$dupe['collid2']]['collectionCode'];
+// 		$targets[$dupe['occid2']]['duplicateid'] = $dupe['duplicateid2'];
+// 	}
+//
+// 	$shownDupe = getShownDuplicate($dupe);
+// 	$shownDupe['duplicateid'] = $dupe['duplicateid'];
+// 	$shownDupe['institutionCode'] = $collections[$dupe['collid']]['institutionCode'];
+// 	$shownDupe['collectionCode'] = $collections[$dupe['collid']]['collectionCode'];
+// 	$options[$dupe['duplicateid']][$dupe['occid']] = $shownDupe;
+// }
 
 $ui_option = 2;
 
@@ -319,9 +397,9 @@ $ui_option = 2;
 			<table class="styledtable table-scroll">
 				<?= getTableHeaders($shownFields, $fieldIgnores)?>
 				<tbody>
-					<?= render_row($target, false, $fieldIgnores) ?>
+					<?= render_row($target, false, $shownFields) ?>
 					<?php foreach ($options[$duplicateId] as $dupe): ?>
-						<?= $dupe['occid'] !== $target['occid']? render_row($dupe, $target['occid'], $fieldIgnores): '' ?>
+						<?= $dupe['occid'] !== $target['occid']? render_row($dupe, $target['occid'], $shownFields): '' ?>
 					<?php endforeach ?>
 				</tbody>
 			</table>
@@ -335,9 +413,9 @@ $ui_option = 2;
 			<?php foreach ($targets as $targetOccid => $target): ?>
 			<?php if(count($options[$target['duplicateid']])): ?>
 				<tbody>
-					<?= render_row($target, false, $fieldIgnores) ?>
-					<?php foreach ($options[$target['duplicateid']] as $dupe): ?>
-						<?= $dupe['occid'] !== $targetOccid? render_row($dupe, $targetOccid, $fieldIgnores): '' ?>
+					<?= render_row($target, false, $shownFields) ?>
+					<?php foreach ($options[$target['duplicateid']] as $dupeOccid => $dupe): ?>
+						<?= $dupeOccid !== $targetOccid? render_row($dupe, $targetOccid, $shownFields): '' ?>
 					<?php endforeach ?>
 					<tr>
 						<td colspan="18" style="height: 1rem"></td>
