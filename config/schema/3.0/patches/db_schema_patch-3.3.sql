809ccb3212735ad9b4d8662bc71f613bdfbe52a9
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2025-09-06 22:47:23 -0500

    Schema update to work for Bcrypt hashes

--- config/schema/3.0/patches/db_schema_patch-3.3.sql
+++ config/schema/3.0/patches/db_schema_patch-3.3.sql
@@ -60,3 +60,7 @@ ALTER TABLE `portalindex`
   ADD COLUMN `lastContact` TIMESTAMP NULL AFTER `notes`,
   ADD COLUMN `modifiedTimestamp` TIMESTAMP NULL AFTER `lastContact`,
   ADD COLUMN `apiVersion` VARCHAR(45) NULL AFTER `symbiotaVersion`;
+
+#Bcrypt hash storage needs 60 characters
+ALTER TABLE `users`
+  CHANGE COLUMN `password` `password` VARCHAR(60) NOT NULL;
