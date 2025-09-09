e71ae4b36abe80ce3f31a6ce150ef2ea4422e282
MuchQuak <loganpwilt@gmail.com>
2025-09-09 12:00:05 -0700

    Pull changes from #2829 closes #2825

--- config/schema/3.0/patches/db_schema_patch-3.4.sql
+++ config/schema/3.0/patches/db_schema_patch-3.4.sql
@@ -252,3 +252,8 @@ ALTER TABLE 'uploadspectemp'
   ADD COLUMN 'slideProperties' TEXT,
   ADD COLUMN 'geologicalContextID' TEXT;
 
+# CURRENT: title varchar(50) NOT NULL
+# While unlikely possible combination of omocurrences
+# recordedBy(varchar(255)) + ' '  + recordNumber(varchar(45)) + ' ' eventDate(10 chars after string conversion)
+# would result in ~312 chars maximum which breaks this fields maximum characters
+ALTER TABLE omoccurduplicates MODIFY title TEXT NOT NULL;
