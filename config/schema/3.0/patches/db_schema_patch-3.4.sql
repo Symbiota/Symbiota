f5f3f3bf78a22c46a00d686f264faface0e1140a
Nikita Salikov <nsalikov@asu.edu>
2025-09-10 09:41:52 -0700

    remove already existing index

--- config/schema/3.0/patches/db_schema_patch-3.4.sql
+++ config/schema/3.0/patches/db_schema_patch-3.4.sql
@@ -1,5 +1,3 @@
-#fields and indexes for search by polygons
+#field for search by polygons
 ALTER TABLE geographicthesaurus
     ADD COLUMN isSearchable TINYINT(1) NOT NULL DEFAULT 0;
-
-ALTER TABLE omoccurpoints ADD SPATIAL INDEX(lngLatPoint);
