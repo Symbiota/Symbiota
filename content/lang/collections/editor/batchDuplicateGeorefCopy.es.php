bc7355c5fef81c10edca2466c54a7eb7eee8adb2
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-09-23 08:53:44 -0700

    Add missing translations

--- content/lang/collections/editor/batchDuplicateGeorefCopy.es.php
+++ content/lang/collections/editor/batchDuplicateGeorefCopy.es.php
@@ -4,12 +4,19 @@
 Language: Español
 ------------------
 */
-$LANG['BATCH_DUPLICATE_HARVESTER'] = 'Recopilador de datos duplicados';
-$LANG['DUPLICATE_SEARCH_CRITERIA'] = 'Criterios de búsqueda duplicados';
-$LANG['MISSING_LAT_LNG'] = 'Solo mostrar especímenes de mi colección sin latitud y longitud';
-$LANG['HIDE_EXACT_MATCHES'] = 'Mostrar solo duplicados con georreferencias diferentes a la muestra de destino';
+
+$LANG['BATCH_DUPLICATE_HARVESTER'] = 'Recolector de Datos Duplicados';
+$LANG['MUST_BATCH_LINK_DUPLICATES'] = 'Para que esta herramienta encuentre georreferencias duplicadas, sus especímenes ya deben estar vinculados como 
+    duplicados a otros especímenes en el portal. Se recomienda ejecutar el proceso "Vincular duplicados de especímenes por lotes" en las Herramientas de 
+    Agrupamiento de Duplicados antes de usar el recolector de georreferencias.';
+$LANG['DUPLICATE_SEARCH_CRITERIA'] = 'Criterios de búsqueda de duplicados';
+$LANG['MISSING_LAT_LNG'] = 'Mostrar solo especímenes de mi colección sin latitud ni longitud';
+$LANG['HIDE_EXACT_MATCHES'] = 'Mostrar solo duplicados con georreferencias diferentes a las del espécimen objetivo';
 $LANG['NO_DUPLICATES'] = 'No hay clústeres duplicados que coincidan con esta búsqueda';
 $LANG['FILTER_COLLECTIONS'] = 'Filtrar colecciones';
 $LANG['COPY_DUPLICATE_DATA'] = 'Copiar datos duplicados';
-$LANG['COPY_DUPLICATE_DATA_EXPLANATION'] = 'Al hacer clic en el botón de arriba, se reemplazarán los datos de georreferencia en el registro de destino (gris oscuro) con los datos del registro duplicado marcado. Se reemplazarán los siguientes campos: decimalLatitude, decimalLongitude, geodeticDatum, footprintWKT, coordinateUncertaintyInMeters, georeferencedBy, georeferenceRemarks, georeferenceSources, georeferenceProtocol, georeferenceVerificationStatus';
+$LANG['COPY_DUPLICATE_DATA_EXPLANATION'] = 'Al hacer clic en el botón superior, se reemplazarán los datos de georreferencia del registro de destino (gris oscuro) con los datos del registro duplicado seleccionado. Se reemplazarán los siguientes campos: decimalLatitude, decimalLongitude, geodeticDatum, footprintWKT, coordinateUncertaintyInMeters, georeferencedBy, georeferenceRemarks, georeferenceSources, georeferenceProtocol, georeferenceVerificationStatus';
+$LANG['ENABLE_AUTO_CHECK'] = 'Seleccionar duplicados automáticamente con una sola opción (nota: los duplicados dentro de su colección no se seleccionarán automáticamente)';
+$LANG['SEARCH_TO_SEE_DUPLICATES'] = 'Ingrese la búsqueda de valores para ver posibles duplicados';
+
 ?>
--- content/lang/collections/editor/batchDuplicateGeorefCopy.fr.php
+++ content/lang/collections/editor/batchDuplicateGeorefCopy.fr.php
@@ -4,12 +4,19 @@
 Language: Français (French)
 ------------------
 */
-$LANG['BATCH_DUPLICATE_HARVESTER'] = 'Récupérateur de données en double';
-$LANG['DUPLICATE_SEARCH_CRITERIA'] = 'Critères de recherche en double';
-$LANG['MISSING_LAT_LNG'] = 'Ne montrez que les spécimens de ma collection sans latitude ni longitude';
-$LANG['HIDE_EXACT_MATCHES'] = 'Afficher uniquement les doublons avec des géoréférences différentes de l\'échantillon cible';
-$LANG['NO_DUPLICATES'] = 'Il n\'y a pas de clusters en double qui correspondent à cette recherche';
-$LANG['FILTER_COLLECTIONS'] = 'Collections de filtres';
-$LANG['COPY_DUPLICATE_DATA'] = 'Copier les données en double';
-$LANG['COPY_DUPLICATE_DATA_EXPLANATION'] = 'En cliquant sur le bouton ci-dessus, vous remplacerez les données de géoréférence de l\'enregistrement cible (gris foncé) par les données de l\'enregistrement en double coché. Les champs suivants seront remplacés : decimalLatitude, decimalLongitude, geodeticDatum, footprintWKT, coordinateUncertaintyInMeters, georeferencedBy, georeferenceRemarks, georeferenceSources, georeferenceProtocol, georeferenceVerificationStatus';
+
+$LANG['BATCH_DUPLICATE_HARVESTER'] = 'Moissonneuse de Données en Double';
+$LANG['MUST_BATCH_LINK_DUPLICATES'] = 'Pour que cet outil puisse trouver les doublons de géoréférences, vos spécimens doivent déjà 
+    être liés en tant que doublons à d\'autres spécimens du portail. Il est recommandé d\'exécuter la procédure «Lier les doublons 
+    de spécimens par lots» dans les Outils de Regroupement des Doublons avant d\'utiliser la moissonneuse de données en double.';
+$LANG['DUPLICATE_SEARCH_CRITERIA'] = 'Critères de recherche des doublons';
+$LANG['MISSING_LAT_LNG'] = 'Afficher uniquement les spécimens de ma collection sans latitude ni longitude';
+$LANG['HIDE_EXACT_MATCHES'] = 'Afficher uniquement les doublons dont les géoréférences sont différentes de celles du spécimen cible';
+$LANG['NO_DUPLICATES'] = 'Aucun cluster en double ne correspond à cette recherche.';
+$LANG['FILTER_COLLECTIONS'] = 'Filtrer les collections.';
+$LANG['COPY_DUPLICATE_DATA'] = 'Copier les données en double.';
+$LANG['COPY_DUPLICATE_DATA_EXPLANATION'] = 'Cliquer sur le bouton ci-dessus remplacera les données de géoréférencement de l\'enregistrement cible (gris foncé) par celles de l\'enregistrement en double vérifié. Les champs suivants seront remplacés: decimalLatitude, decimalLongitude, geodeticDatum, footprintWKT, coordinateUncertaintyInMeters, georeferencedBy, georeferenceRemarks, georeferenceSources, georeferenceProtocol, georeferenceVerificationStatus.';
+$LANG['ENABLE_AUTO_CHECK'] = 'Sélectionner automatiquement les doublons avec une seule option (Remarque: les doublons de votre collection ne seront pas sélectionnés automatiquement)';
+$LANG['SEARCH_TO_SEE_DUPLICATES'] = 'Saisir des valeurs de recherche pour afficher les doublons potentiels';
+
 ?>
