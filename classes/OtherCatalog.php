ad6d7b0825ba3f559a07d4715eb114bcc04f230c
Katie Pearson <katelin.d.pearson24@gmail.com>
2025-11-18 16:42:43 -0800

    Reduced batch size to 10000 to reduce wait before first entry

--- classes/OtherCatalog.php
+++ classes/OtherCatalog.php
@@ -1,7 +1,7 @@
 <?php
 class OtherCatalog {
     private $conn;
-    private $batchSize = 25000;
+    private $batchSize = 10000;
     private $modifiedUID;
     private $count = 0;
     private $totalProcessed = 0;
