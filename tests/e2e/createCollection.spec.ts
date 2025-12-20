16c12149e7ef90e73208aecc8f5077a7f397cfa9
MuchQuak <loganpwilt@gmail.com>
2025-12-20 14:32:20 -0800

    Add collection fixture

--- tests/e2e/createCollection.spec.ts
+++ tests/e2e/createCollection.spec.ts
@@ -1,17 +1,19 @@
 import { expect, mergeTests } from '@playwright/test';
-import { test as testDB } from './fixtures/db';
+import { test as testCollection } from './fixtures/collection';
 import { test as testWithAdmin } from './fixtures/adminLogin';
+import { CollectionCreatePage } from './pages/CollectionCreatePage';
 
-const { CollectionCreatePage } = require('./pages/CollectionCreatePage');
+const test = mergeTests(testCollection, testWithAdmin);
 
-const test = mergeTests(testDB, testWithAdmin);
+test.beforeEach(async ({ adminLogin }) => await adminLogin.expectLoggedIn());
 
-test.beforeEach(async ({ page, adminLogin }) => {
-	await adminLogin.expectLoggedIn();
+test.afterEach(async ({ collection, browserName }) => {
+	let collId = await collection.getByName(browserName + ' CI Collection')
+	await collection.deleteByCollId(collId);
 });
 
-test('Create an Collection', async ({ page, DB }, workerInfo) => {
-	const collectionName = workerInfo.project.name + ' CI Collection';
+test('Create an Collection', async ({ page, browserName }) => {
+	const collectionName = browserName + ' CI Collection';
 
 	let collData = {
 		institutionCode: 'SYMB',
@@ -22,15 +24,7 @@ test('Create an Collection', async ({ page, DB }, workerInfo) => {
 	let collectionCreatePage = new CollectionCreatePage(page);
 	await collectionCreatePage.goto();
 	await collectionCreatePage.setMany(collData);
-	await collectionCreatePage.setToLiveManaged(collData);
+	await collectionCreatePage.setToLiveManaged();
 	await collectionCreatePage.submitCreate();
 	await expect(page.getByText('New collection added successfully!')).toBeVisible();	
 })
-
-test.afterEach(async ({ DB }, workerInfo) => {
-	const [ search ] = await DB.execute("SELECT collId from omcollections where collectionName = ?", [workerInfo.project.name + ' CI Collection']);
-	if(search.length > 0) {
-		await DB.execute('DELETE from omcollectionstats where collId = ?', [ search[0].collId ]);
-		await DB.execute('DELETE from omcollections where collId = ?', [ search[0].collId ]);
-	}
-});
