f9ffb887a45819bba8152e0c69a55896f62c5ee9
MuchQuak <loganpwilt@gmail.com>
2025-12-19 22:44:10 -0800

    Add e2e test for creating new record

--- tests/e2e/occurrenceEditor.spec.ts
+++ tests/e2e/occurrenceEditor.spec.ts
@@ -5,22 +5,43 @@ import { test as testWithAdmin } from './fixtures/adminLogin';
 const { OccurrenceEditorPage } = require('./pages/OccurrenceEditorPage');
 
 const test = mergeTests(testDB, testWithAdmin);
+let collId = null;
+
+test.beforeAll(async ({ DB }, workerinfo) => {
+	const globalCollectionName = workerinfo.project.name + ' CI Global Collection';
+	const workerCode = workerinfo.project.name.slice(0, 4);
+
+	const insert = await DB.execute(
+		"INSERT omcollections (institutionCode, collectionCode, collectionName, managementType) VALUES (?, ?, ?, ?)",
+		['SYMB', workerCode, globalCollectionName, 'Live Data']
+	);
+
+	const [ search ] = await DB.execute("SELECT collId from omcollections where collectionName = ?", [globalCollectionName]);
+
+	if(search.length > 0) {
+		collId = search[0].collId;
+	}
+})
 
 test.beforeEach(async ({ adminLogin }) => {
 	await adminLogin.expectLoggedIn();
 });
 
-// test('Create an occurrence record', async ({ page }) => {
-// 	const collId = 0; 
-// 	const inputs = {
-// 		catalognumber: '000001',
-// 	};
-//
-// 	let occurrenceEditor = new OccurrenceEditorPage(page);
-// 	await occurrenceEditor.gotoNew(collId);
-// 	await occurrenceEditor.setMany(inputs);
-// 	await page.locator('input[name=gotomode][value="0"]').click({force: true});
-// 	await occurrenceEditor.submitNewRecord();
-// 	await expect(page.getByText('Public Display')).toBeVisible();	
-// 	await occurrenceEditor.checkMany(inputs)
-// })
+test.afterAll(async ({ DB }) => {
+	await DB.execute('DELETE from omcollectionstats where collId = ?', [ collId ]);
+	await DB.execute('DELETE from omcollections where collId = ?', [ collId ]);
+});
+
+test('Create an occurrence record', async ({ page }) => {
+	const inputs = {
+		catalognumber: '000001',
+	};
+
+	let occurrenceEditor = new OccurrenceEditorPage(page);
+	await occurrenceEditor.gotoNew(collId);
+	await occurrenceEditor.setMany(inputs);
+	await page.locator('input[name=gotomode][value="0"]').click({force: true});
+	await occurrenceEditor.submitNewRecord();
+	await expect(page.getByText('Public Display')).toBeVisible();	
+	await occurrenceEditor.checkMany(inputs)
+})
