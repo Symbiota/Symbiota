646cd1dff45a61d843bbdd4a0b02d1c3ef64e6e5
MuchQuak <loganpwilt@gmail.com>
2026-01-21 12:10:56 -0800

    Add storage tracking for audio and misc

--- classes/Media.php
+++ classes/Media.php
@@ -608,8 +608,10 @@ class Media {
 					self::update_metadata($metadata, $media_metadata['mediaID'], $conn);
 				} elseif($media_type === MediaType::Audio) {
 					$storage->upload($file);
+					$createdFilepaths['originalUrl'] = $storage->getDirPath($file);
 				} elseif($media_type === MediaType::Misc) {
 					$storage->upload($file);
+					$createdFilepaths['originalUrl'] = $storage->getDirPath($file);
 				}
 			}
 
