2cb6ed0596b48e94cee2d3f0030d3dbb066fbf95
MuchQuak <loganpwilt@gmail.com>
2026-02-16 15:54:37 -0800

    Add Suite Abstraction, Abstract mediaForm and occurForm

--- tests/e2e/pages/OccurrenceEditorPage.ts
+++ tests/e2e/pages/OccurrenceEditorPage.ts
@@ -1,4 +1,7 @@
-import type { Page } from '@playwright/test';
+import { expect, type Page } from '@playwright/test';
+import { MediaForm } from '../forms/MediaForm';
+import { getSuite, Suite } from '../types/Suite';
+import { OccurrenceForm } from '../forms/OccurrenceForm';
 
 export enum OccurrenceEditorTab {
 	Occurrence = 'occTab',
@@ -8,9 +11,40 @@ export enum OccurrenceEditorTab {
 	Admin = 'adminTab'
 }
 
-export class OccurrenceEditorPage {
-	constructor(public readonly page: Page) {}
+export abstract class OccurrenceEditorPage {
+	occurForm: OccurrenceForm;
+	mediaForm: MediaForm;
 
+	constructor(public readonly page: Page) {
+		this.mediaForm = MediaForm.make(page);
+		this.occurForm = new OccurrenceForm(page);
+	}
+
+	static make(page: Page): OccurrenceEditorPage {
+		switch(getSuite()) {
+			case Suite.Laravel:
+				throw new Error('ERROR: ' + Suite.Laravel + ' SUITE: NOT IMPLEMENTED');
+			default:
+				return new SymbOccurrenceEditorPage(page);
+		}
+	}
+
+	abstract gotoNew(collId: number): Promise<void>;
+	abstract gotoRecord(collId: number, occId: number): Promise<void>;
+	abstract gotoImageSubmit(collId: number): Promise<void>;
+	abstract gotoSkeletalSubmit(collId: number): Promise<void>;
+	abstract gotoTab(newTab: OccurrenceEditorTab): Promise<void>;
+	abstract getSkeletalOccid(): Promise<Number>;
+	abstract getSkeletalImageOccid(): Promise<Number>;
+
+	abstract swapToMediaEnterUrl(): Promise<void>;
+
+	abstract setGotoRecord(): Promise<void>;
+
+	abstract checkRecordSuccess(): Promise<void>;
+}
+
+export class SymbOccurrenceEditorPage extends OccurrenceEditorPage {
 	async gotoNew(collId: number) {
 		await this.page.goto('collections/editor/occurrenceeditor.php?gotomode=1&collid=' + collId);
 	}
@@ -34,5 +68,28 @@ export class OccurrenceEditorPage {
 		if(OccurrenceEditorTab.Occurrence != newTab) {
 			await this.page.getByText('Loading...').waitFor({ state: "detached" });
 		}
+	}
+
+	async getSkeletalOccid() {
+		const newRecordLink = await this.page.waitForSelector('div[id="occurlistdiv"] a[id*="a-"]', { state: 'attached' });
+		const id = await newRecordLink.getAttribute('id');
+		return id? parseInt(id.replace('a-', '')): 0;
+	}
+
+	async getSkeletalImageOccid() {
+		const newRecordLink = this.page.locator('a[href*="occurrenceeditor.php"]');
+		return parseInt(await newRecordLink.innerText());
+	}
+
+	async swapToMediaEnterUrl() {
+		await this.page.getByText("Enter Url").click({force: true});
+	}
+
+	async setGotoRecord() {
+		await this.page.locator('input[name=gotomode][value="0"]').click({force: true});
+	}
+
+	async checkRecordSuccess() {
+		await expect(this.page.getByText('Public Display')).toBeVisible();
 	}
 }
