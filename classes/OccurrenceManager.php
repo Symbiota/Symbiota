833e00374562c5609752c5ddf05a087b724bf024
Edward Gilbert <egbot@asu.edu>
2026-03-12 09:42:33 -0400

    Allow multiple short word search
    - Allows for searching "fe de la" without fatal error

--- classes/OccurrenceManager.php
+++ classes/OccurrenceManager.php
@@ -218,8 +218,12 @@ class OccurrenceManager extends OccurrenceTaxaManager {
 						//BUGFIX: MySQL fullltext index does not include strings shorter than a configurable length (likely < 3)  Need to remove these sub strings from the AGAINST clause
 						$against_values  = preg_replace('/\b[\w]{1,' . ($this->fullTextMinTokenLength-1) . '}\b/u', ' ', $value);
 						$against_values  = trim(preg_replace('/\s+/', ' ', $against_values));
-
-						$tempSqlArr[] = '(MATCH(o.locality) AGAINST("+'.str_replace(' ', ' +', $against_values).'" IN BOOLEAN MODE) AND o.locality LIKE "%'.$value.'%")';
+						if($against_values){
+							$tempSqlArr[] = '(MATCH(o.locality) AGAINST("+' . str_replace(' ', ' +', $against_values) . '" IN BOOLEAN MODE) AND o.locality LIKE "%' . $value . '%")';
+						}
+						else{
+							$tempSqlArr[] = '(o.locality LIKE "%' . $value . '%")';
+						}
 					}
 					else{
 						$singleWords[] = $value;
@@ -227,7 +231,7 @@ class OccurrenceManager extends OccurrenceTaxaManager {
 					$tempTermArr[] = $value;
 				}
 			}
-			//TODO: handle single words that are less than ft_min_token_length
+			//TODO: should we handle single words that are 2 characters or less
 			if($singleWords) $tempSqlArr[] = '(MATCH(o.locality) AGAINST("'.implode(' ', $singleWords).'"))';
 			$sqlWhere .= 'AND ('.implode(' OR ', $tempSqlArr).') ';
 			if($tempTermArr) $this->displaySearchArr[] = implode(' OR ', $tempTermArr);
