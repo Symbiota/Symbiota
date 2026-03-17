375905fe5fcc7fc81c96185841068c3202b8dc11
atticus29 <mark.aaron.fisher@gmail.com>
2026-03-17 16:13:17 -0700

    remove cruft and add clarifying comments

--- tests/e2e/fixtures/taxon.ts
+++ tests/e2e/fixtures/taxon.ts
@@ -4,87 +4,13 @@ import mysql from "mysql2/promise";
 class Taxon {
   constructor(public readonly conn: mysql.Connection) {}
 
-  // Placeholder method until other methods are implemented
+  // Placeholder method until other methods are implemented. 
+  // Basically, this is here currently to follow the pattern of how other pages provide a DB connection
   placeholder() {
     return this.conn;
   }
-
-  //   async getByName(taxonName: string) {
-  //     const [search] = await this.conn.execute(
-  //       "SELECT collId from omcollections where collectionName = ?",
-  //       [collectionName],
-  //     );
-  //     if (search.length > 0) {
-  //       return search[0].collId;
-  //     } else {
-  //       return 0;
-  //     }
-  //   }
-
-  //   async insertBasic(
-  //     collectionName: string,
-  //     managementType: string = "Live Data",
-  //     collectionType: string = "Preserved Specimens",
-  //   ): Promise<number> {
-  //     await this.conn.execute(
-  //       "INSERT omcollections (institutionCode, collectionCode, collectionName, managementType, collType) VALUES (?, uuid(), ?, ?, ?)",
-  //       ["SYMB", collectionName, managementType, collectionType],
-  //     );
-  //     let collId = 0;
-
-  //     let result = await this.conn.execute("SELECT LAST_INSERT_ID() as id");
-  //     if (result.length > 0 && result[0].length > 0) {
-  //       collId = result[0][0].id;
-  //     }
-
-  //     // Inserting with recordCnt 1 so it displays where expected
-  //     await this.conn.execute(
-  //       "INSERT omcollectionstats(collId, recordCnt) VALUES (?, 1)",
-  //       [collId],
-  //     );
-
-  //     return collId;
-  //   }
-
-  //   async getOrCreate(collectionName) {
-  //     let collid = await this.getByName(collectionName);
-
-  //     if (!collid) {
-  //       await this.insertBasic(collectionName);
-  //       collid = await this.getByName(collectionName);
-  //     }
-
-  //     return collid;
-  //   }
-
-  //   async resetCollection(collId) {
-  //     await this.conn.execute(
-  //       "DELETE from media where occid in (select occid from omoccurrences where collId = ?)",
-  //       [collId],
-  //     );
-  //     await this.conn.execute("DELETE from omoccurrences where collId = ?", [
-  //       collId,
-  //     ]);
-  //   }
-
-  //   async deleteByCollId(collId) {
-  //     await this.conn.execute(
-  //       "DELETE from media where occid in (select occid from omoccurrences where collId = ?)",
-  //       [collId],
-  //     );
-  //     await this.conn.execute("DELETE from omoccurrences where collId = ?", [
-  //       collId,
-  //     ]);
-  //     await this.conn.execute("DELETE from omcollectionstats where collId = ?", [
-  //       collId,
-  //     ]);
-  //     await this.conn.execute("DELETE from omcollections where collId = ?", [
-  //       collId,
-  //     ]);
-  //   }
 }
 
-// Extend basic test by providing a "todoPage" fixture.
 const test = base.extend<{ taxon: Taxon; taxonId: number }>({
   taxon: async ({ DB }, use) => {
     await use(new Taxon(DB));
