b6063afbbff9608f77d31211be3cec33b28ba398
Kelsey Yule <kelsey.yule@gmail.com>
2026-04-21 14:42:40 -0700

    references schema patch

---
+++ config/schema/3.0/patches/db_schema_patch-3.4-references.sql
@@ -0,0 +1,54 @@
+
+CREATE TABLE referenceobject_new (
+    refid INT(11) NOT NULL AUTO_INCREMENT,
+    bibliographicCitation VARCHAR(1000),
+    identifier VARCHAR(100),
+    title VARCHAR(255),
+    creator VARCHAR(255),
+    date VARCHAR(45),
+    source VARCHAR(255),
+    description TEXT,
+    subject VARCHAR(255),
+    language VARCHAR(45),
+    rights VARCHAR(255),
+    type VARCHAR(100),
+    taxonRemarks TEXT,
+    datasetID INT(11),
+    modifiedByUid INT(11),
+    modifiedTimestamp DATETIME,
+    initialTimestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (refid)
+);
+
+INSERT INTO referenceobject_new (
+    refid,
+    title,
+    creator,
+    date,
+    source,
+    bibliographicCitation,
+    identifier,
+	modifiedByUid,
+    modifiedTimestamp,
+    initialTimestamp
+)
+SELECT
+    refid,
+    title,
+    cheatauthors,
+    pubdate,
+    secondarytitle,
+    cheatcitation,
+    guid,
+    modifieduid,
+    modifiedtimestamp,
+    initialtimestamp
+FROM referenceobject;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+RENAME TABLE referenceobject TO referenceobject_old,
+             referenceobject_new TO referenceobject;
+             
+SET FOREIGN_KEY_CHECKS = 1;
+
