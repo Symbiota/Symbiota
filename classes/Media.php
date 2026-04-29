e2a73b985980c1e654ae9d1287912cb36110c2d3
MuchQuak <loganpwilt@gmail.com>
2026-04-29 10:05:59 -0700

    Impl for getAllowedMime

--- classes/Media.php
+++ classes/Media.php
@@ -226,12 +226,12 @@ class Media {
 			return is_array($mime) && count($mime) > 0? $mime[0]: $mime;
 		} else if(is_array($mime)) {
 			foreach($mime as $type) {
-				if(in_array($type, $GLOBALS['ALLOWED_MEDIA_MIME_TYPES'])) {
+				if(UploadUtil::mimeAllowed($type, $GLOBALS['ALLOWED_MEDIA_MIME_TYPES'])) {
 					return $type;
 				}
 			}
 		} else {
-			if(in_array($mime, $GLOBALS['ALLOWED_MEDIA_MIME_TYPES'])) {
+			if(UploadUtil::mimeAllowed($mime, $GLOBALS['ALLOWED_MEDIA_MIME_TYPES'])) {
 				return $mime;
 			}
 		}
