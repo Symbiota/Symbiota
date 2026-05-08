5b57dcb46164b0d5ff8dace996cab372b2214cbb
Nikita Salikov <nsalikov@asu.edu>
2026-05-08 11:03:02 -0700

    added default server values

--- geothesaurus/polygonindexhandler.php
+++ geothesaurus/polygonindexhandler.php
@@ -1,11 +1,17 @@
 <?php
 /*
  * For Future page:
- * php polygonindexhandler.php pending 10
- * php polygonindexhandler.php 123,456
+ * php geothesaurus/polygonindexhandler.php pending 10
+ * php geothesaurus/polygonindexhandler.php 123,456
  */
 
-include_once('../config/symbini.php');
+
+$_SERVER['SERVER_PORT'] = $_SERVER['SERVER_PORT'] ?? 80;
+$_SERVER['HTTPS'] = $_SERVER['HTTPS'] ?? 'off';
+$_SERVER['HTTP_HOST'] = $_SERVER['HTTP_HOST'] ?? 'localhost';
+$_SERVER['SERVER_NAME'] = $_SERVER['SERVER_NAME'] ?? $_SERVER['HTTP_HOST'];
+
+include_once(__DIR__ . '/../config/symbini.php');
 include_once($SERVER_ROOT . '/classes/OccurrencePolygonIndex.php');
 
 $mode = $argv[1] ?? 'pending';
