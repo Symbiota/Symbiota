ed18718f8e0bfb04b2a7729e3cbd95a76692a604
Edward Gilbert <egbot@asu.edu>
2026-09-21 15:02:24 -0400

    Logic to convert omoccurpoints to InnoDB
    - Drop and recreate omoccurpoints with cascading FK constraint on omoccurrences, and then rebuild via omoccurrence table
    - Remove redundant `point` from omoccudpoints table due to no longer being used within code
    - Remove `point` table from INSERT and UPDATE omoccurrences triggers
    - Drop omoccurrences delete trigger due to cascading FK constraint within omoccurpoints handling that action
    - Remove swap_wkt_coords function, which was only used within db_schema_patch-3.2.sql

--- config/schema/3.0/patches/db_schema_patch-3.5.sql
+++ config/schema/3.0/patches/db_schema_patch-3.5.sql
@@ -55,4 +55,65 @@ INSERT INTO `ctcontrolvocabterm` (`cvID`, `term`)
   SELECT cvID, "Closed" FROM ctcontrolvocab WHERE tableName = "omoccurrences" AND fieldName = "processingStatus";
 
 
+#Convert occurrence points spatial indexing table from MyISAM to InnoDB
+DROP TABLES omoccurpoints;
+
+CREATE TABLE `omoccurpoints` (
+  `geoID` int(11) NOT NULL AUTO_INCREMENT,
+  `occid` int(10) UNSIGNED NOT NULL,
+  `lngLatPoint` point NOT NULL,
+  `errradiuspoly` polygon DEFAULT NULL,
+  `footprintpoly` polygon DEFAULT NULL,
+  `initialtimestamp` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`geoID`),
+  UNIQUE KEY `IX_omoccurpoints_occid` (`occid`),
+  SPATIAL KEY `IX_omoccurpoints_latLngPoint` (`lngLatPoint`)
+) ENGINE=InnoDB;
+
+ALTER TABLE `omoccurpoints` 
+ADD CONSTRAINT `FK_oomoccurpoints_occid`  FOREIGN KEY (`occid`)  REFERENCES `omoccurrences` (`occid`)  ON DELETE CASCADE  ON UPDATE CASCADE;
+
+INSERT INTO omoccurpoints (occid, lngLatPoint) 
+  SELECT occid, POINT(decimalLongitude, decimalLatitude)
+  FROM omoccurrences
+  WHERE decimalLatitude between -90 and 90 and decimalLongitude between -180 and 180;
+
+
+#Remove duplicate uploadspectemp indexes 
+ALTER TABLE `uploadspectemp` 
+  DROP INDEX `Index_uploadspec_othercatalognumbers` ,
+  DROP INDEX `Index_uploadspec_catalognumber` ,
+  DROP INDEX `Index_uploadspec_sciname` ,
+  DROP INDEX `Index_uploadspectemp_dbpk` ,
+  DROP INDEX `Index_uploadspectemp_occid` ;
+
+ALTER TABLE `uploadspectemp` 
+  ADD INDEX `IX_uploadspectemp_occid` (`occid` ASC, `collid` ASC);
+
+
+#Remove function that was only used within db_schema_3.2.sql db patch 
+DROP FUNCTION `swap_wkt_coords`;
+
+
+#Trigger adjustments to omoccurrences table to adjust to InnoDB omoccurpoints table change
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` TRIGGER `omoccurrences_insert` 
+  AFTER INSERT ON `omoccurrences` FOR EACH ROW BEGIN IF NEW.`decimalLatitude` IS NOT NULL AND NEW.`decimalLongitude` IS NOT NULL 
+  THEN INSERT INTO omoccurpoints (`occid`, `lngLatPoint`) VALUES (NEW.`occid`, Point(NEW.`decimalLongitude`, NEW.`decimalLatitude`)); 
+  END IF; END$$
+CREATE DEFINER=`root`@`localhost` TRIGGER `omoccurrences_update` 
+  AFTER UPDATE ON `omoccurrences` FOR EACH ROW BEGIN 
+  IF NEW.`decimalLatitude` IS NOT NULL AND NEW.`decimalLongitude` IS NOT NULL 
+  THEN IF OLD.`decimalLatitude` IS NULL OR (NEW.`decimalLatitude` != OLD.`decimalLatitude` AND NEW.`decimalLongitude` != OLD.`decimalLongitude`) 
+  THEN IF EXISTS (SELECT `occid` FROM omoccurpoints WHERE `occid`=NEW.`occid`) 
+  THEN UPDATE omoccurpoints SET `lngLatPoint` = Point(NEW.`decimalLongitude`, NEW.`decimalLatitude`) WHERE `occid` = NEW.`occid`; 
+  ELSE INSERT INTO omoccurpoints (`occid`,`lngLatPoint`) VALUES (NEW.`occid`, Point(NEW.`decimalLongitude`, NEW.`decimalLatitude`)); 
+  END IF; END IF; 
+  ELSE IF OLD.`decimalLatitude` IS NOT NULL THEN DELETE FROM omoccurpoints WHERE `occid` = NEW.`occid`; 
+  END IF; END IF; END$$
+DROP TRIGGER IF EXISTS `omoccurrences_delete` $$
+DELIMITER ;
+
+
+
 
